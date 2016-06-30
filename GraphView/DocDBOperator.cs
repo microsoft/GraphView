ef80afc4dd5a95285fc20fcb20274908768414d5
DengMinghua <gdsgdmh@126.com>
2016-06-30 13:49:44 +0800

    Add an interface for both selecting and munipulating part

---
+++ GraphView/DocDBOperator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphView
+{
+    interface DocDBOperator
+    {
+        void Open();
+        void Close();
+        object Next();
+    }
+
+    abstract class DocDBOperatorProcessor : DocDBOperator
+    {
+        public bool statue;
+        public void Open()
+        {
+            statue = true;
+        }
+        public void Close()
+        {
+            statue = false;
+        }
+        public abstract object Next();
+    }
+}
