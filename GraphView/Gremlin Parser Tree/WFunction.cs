09297a92abc3f16b49d49d8bc6e30e48d5fb3fa4
DengMinghua <gdsgdmh@126.com>
2016-07-22 11:01:01 +0800

    Refactor some codes.

--- GraphView/Gremlin Parser Tree/WFunction.cs
+++ GraphView/Gremlin Parser Tree/WFunction.cs
@@ -11,6 +11,22 @@ namespace GraphView
         internal int KeywordIndex;
         internal WParameters Parameters;
         internal WFragment Fragment;
+
+        internal void AddNewAlias(string alias, ref GraphViewGremlinSematicAnalyser.Context context, string predicates ="")
+        {
+            context.InternalAliasList.Add(alias);
+            context.AliasPredicates.Add(new List<string>());
+            if (alias[0] == 'N') context.NodeCount++;
+            else context.EdgeCount++;
+            if (predicates != "")
+                context.AliasPredicates.Last().Add(predicates);
+        }
+
+        internal void ChangePrimaryAlias(string alias, ref GraphViewGremlinSematicAnalyser.Context context)
+        {
+            context.PrimaryInternalAlias.Clear();
+            context.PrimaryInternalAlias.Add(alias);
+        }
         internal void Transform(ref GraphViewGremlinSematicAnalyser.Context pContext)
         {
             int index;
@@ -23,27 +39,17 @@ namespace GraphView
             {
                 case (int)GraphViewGremlinParser.Keywords.V:
                     SrcNode = "N_" + pContext.NodeCount.ToString();
-                    pContext.PrimaryInternalAlias.Clear();
-                    pContext.PrimaryInternalAlias.Add(SrcNode);
-                    pContext.InternalAliasList.Add(SrcNode);
-                    pContext.AliasPredicates.Add(new List<string>());
-                    pContext.NodeCount++;
+                    AddNewAlias(SrcNode,ref pContext);
+                    ChangePrimaryAlias(SrcNode,ref pContext);
                     break;
                 case (int)GraphViewGremlinParser.Keywords.E:
                     SrcNode = "N_" + pContext.NodeCount.ToString();
-                    pContext.InternalAliasList.Add(SrcNode);
-                    pContext.AliasPredicates.Add(new List<string>());
-                    pContext.NodeCount++;
+                    AddNewAlias(SrcNode, ref pContext);
                     DestNode = "N_" + pContext.NodeCount.ToString();
-                    pContext.InternalAliasList.Add(DestNode);
-                    pContext.AliasPredicates.Add(new List<string>());
-                    pContext.NodeCount++;
+                    AddNewAlias(DestNode, ref pContext);
                     Edge = "E_" + pContext.EdgeCount.ToString();
-                    pContext.InternalAliasList.Add(Edge);
-                    pContext.AliasPredicates.Add(new List<string>());
-                    pContext.EdgeCount++;
-                    pContext.PrimaryInternalAlias.Clear();
-                    pContext.PrimaryInternalAlias.Add(Edge);
+                    AddNewAlias(Edge, ref pContext);
+                    ChangePrimaryAlias(Edge, ref pContext);
                     pContext.Paths.Add((new Tuple<string, string, string>(SrcNode, Edge, DestNode)));
                     break;
                 case (int)GraphViewGremlinParser.Keywords.next:
@@ -87,18 +93,14 @@ namespace GraphView
                     foreach (var para in Parameters.Parameter)
                     {
                         Edge = "E_" + pContext.EdgeCount.ToString();
-                        pContext.InternalAliasList.Add(Edge);
-                        pContext.AliasPredicates.Add(new List<string>());
-                        pContext.EdgeCount++;
+                        AddNewAlias(Edge,ref pContext);
                         index = pContext.InternalAliasList.Count;
                         pContext.AliasPredicates[index - 1].Add(Edge + ".type" + " = " +
                                                                para.QuotedString);
                         foreach (var alias in pContext.PrimaryInternalAlias)
                         {
                             DestNode = "N_" + pContext.NodeCount.ToString();
-                            pContext.InternalAliasList.Add(DestNode);
-                            pContext.AliasPredicates.Add(new List<string>());
-                            pContext.NodeCount++;
+                            AddNewAlias(DestNode,ref pContext);
                             SrcNode = alias;
                             pContext.Paths.Add(new Tuple<string, string, string>(SrcNode, Edge,
                                 DestNode));
@@ -115,18 +117,14 @@ namespace GraphView
                     foreach (var para in Parameters.Parameter)
                     {
                         Edge = "E_" + pContext.EdgeCount.ToString();
-                        pContext.InternalAliasList.Add(Edge);
-                        pContext.AliasPredicates.Add(new List<string>());
-                        pContext.EdgeCount++;
+                        AddNewAlias(Edge,ref pContext);
                         index = pContext.InternalAliasList.Count;
                         pContext.AliasPredicates[index - 1].Add(Edge + ".type" + " = " +
                                                                para.QuotedString);
                         foreach (var alias in pContext.PrimaryInternalAlias)
                         {
                             SrcNode = "N_" + pContext.NodeCount.ToString();
-                            pContext.InternalAliasList.Add(SrcNode);
-                            pContext.AliasPredicates.Add(new List<string>());
-                            pContext.NodeCount++;
+                            AddNewAlias(SrcNode,ref pContext);
                             DestNode = alias;
                             pContext.Paths.Add(new Tuple<string, string, string>(SrcNode, Edge,
                                 DestNode));
@@ -167,9 +165,7 @@ namespace GraphView
                 case (int)GraphViewGremlinParser.Keywords.inE:
                     DestNode = pContext.PrimaryInternalAlias[0];
                     SrcNode = "N_" + pContext.NodeCount.ToString();
-                    pContext.InternalAliasList.Add(SrcNode);
-                    pContext.AliasPredicates.Add(new List<string>());
-                    pContext.NodeCount++;
+                    AddNewAlias(SrcNode,ref pContext);
                     Edge = "E_" + pContext.EdgeCount.ToString();
                     pContext.InternalAliasList.Add(Edge);
                     if (Parameters != null)
@@ -180,8 +176,7 @@ namespace GraphView
                     }
                     else pContext.AliasPredicates.Add(new List<string>());
                     pContext.EdgeCount++;
-                    pContext.PrimaryInternalAlias.Clear();
-                    pContext.PrimaryInternalAlias.Add(Edge);
+                    ChangePrimaryAlias(Edge,ref pContext);
                     foreach (var alias in pContext.PrimaryInternalAlias)
                     {
                         Edge = alias;
@@ -192,14 +187,12 @@ namespace GraphView
                 case (int)GraphViewGremlinParser.Keywords.inV:
                     Edge = pContext.PrimaryInternalAlias[0];
                     var ExistInPath = pContext.Paths.Find(p => p.Item2 == Edge);
-                    pContext.PrimaryInternalAlias.Clear();
-                    pContext.PrimaryInternalAlias.Add(ExistInPath.Item3);
+                    ChangePrimaryAlias(ExistInPath.Item3,ref pContext);
                     break;
                 case (int)GraphViewGremlinParser.Keywords.outV:
                     Edge = pContext.PrimaryInternalAlias[0];
                     var ExistOutPath = pContext.Paths.Find(p => p.Item2 == Edge);
-                    pContext.PrimaryInternalAlias.Clear();
-                    pContext.PrimaryInternalAlias.Add(ExistOutPath.Item1);
+                    ChangePrimaryAlias(ExistOutPath.Item1, ref pContext);
                     break;
                 case (int)GraphViewGremlinParser.Keywords.As:
                     pContext.ExplictAliasToInternalAlias.Add(Parameters.Parameter[0].QuotedString, pContext.PrimaryInternalAlias[0]);
