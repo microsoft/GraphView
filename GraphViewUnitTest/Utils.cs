2f02daf31e409c4708c491ddc1ea5540b190bfa5
yanjga <yanjga@microsoft.com>
2016-10-12 15:59:15 +0800

    Add metrics to loader

---
+++ GraphViewUnitTest/Utils.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphView;
+
+namespace GraphViewUnitTest
+{
+    public static class DocDBUtils
+    {
+        public static double stdDev(List<double> values)
+        {
+            double ret = 0;
+            if (values.Count() > 0)
+            {
+                //Compute the Average      
+                double avg = values.Average();
+                //Perform the Sum of (value-avg)_2_2      
+                double sum = values.Sum(d => Math.Pow(d - avg, 2));
+                //Put it all together      
+                ret = Math.Sqrt((sum) / (values.Count() - 1));
+            }
+            return ret;
+        }
+
+        public static void ResetCollection(string collection)
+        {
+            GraphViewConnection connection = new GraphViewConnection("https://graphview.documents.azure.com:443/",
+                    "MqQnw4xFu7zEiPSD+4lLKRBQEaQHZcKsjlHxXn2b96pE/XlJ8oePGhjnOofj1eLpUdsfYgEhzhejk2rjH/+EKA==",
+                    "GroupMatch", collection);
+            connection.SetupClient();
+            connection.DocDB_finish = false;
+            connection.BuildUp();
+
+            while (!connection.DocDB_finish)
+                System.Threading.Thread.Sleep(10);
+
+            connection.ResetCollection();
+            connection.DocDB_finish = false;
+            connection.BuildUp();
+
+            while (!connection.DocDB_finish)
+                System.Threading.Thread.Sleep(10);
+        }
+    }
+}
