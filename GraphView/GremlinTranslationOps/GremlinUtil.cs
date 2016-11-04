71a43f97516512c47459dfee9f37a929d1787b84
v-jinjl <v-jinjl@2QZH122>
2016-11-04 15:18:13 +0800

    update GremlinHasOp and add some GremlinUtil function

--- GraphView/GremlinTranslationOps/GremlinUtil.cs
+++ GraphView/GremlinTranslationOps/GremlinUtil.cs
@@ -17,6 +17,11 @@ namespace GraphView.GremlinTranslationOps
             };
         }
 
+        internal static WMultiPartIdentifier GetMultiPartIdentifier(params string[] parts)
+        {
+            return ConvertListToMultiPartIdentifier(parts);
+        }
+
         internal static WMultiPartIdentifier ConvertListToMultiPartIdentifier(string[] parts)
         {
             var MultiIdentifierList = new List<Identifier>();
@@ -40,6 +45,67 @@ namespace GraphView.GremlinTranslationOps
             if (GremlinVar.GetType() != typeof(GremlinEdgeVariable)) {
                 throw new Exception("It's not a GremlinEdgeVariable");
             }
+        }
+
+        internal static BooleanComparisonType GetComparisonTypeFromPredicateType(PredicateType predicateType)
+        {
+            if (predicateType == PredicateType.eq) return BooleanComparisonType.Equals;
+            if (predicateType == PredicateType.neq) return BooleanComparisonType.NotEqualToExclamation;
+            if (predicateType == PredicateType.lt) return BooleanComparisonType.LessThan;
+            if (predicateType == PredicateType.lte) return BooleanComparisonType.LessThanOrEqualTo;
+            if (predicateType == PredicateType.gt) return BooleanComparisonType.GreaterThan;
+            if (predicateType == PredicateType.gte) return BooleanComparisonType.GreaterThanOrEqualTo;
+            throw new Exception("Error: GetComparisonTypeFromPredicateType");
+        }
+
+        internal static WValueExpression GetValueExpression(object value)
+        {
+            if (value.GetType() == typeof(string))
+            {
+                return new WValueExpression(value as string, true);
+            }
+            else
+            {
+                return new WValueExpression(value as string, false);
+            }
+        }
+
+        internal static WBooleanComparisonExpression GetBooleanComparisonExpression(GremlinVariable gremlinVar,
+                                                                                     string key, object value)
+        {
+            WMultiPartIdentifier MultiIdentifierValue = GetMultiPartIdentifier(gremlinVar.VariableName, key);
+            WScalarExpression ValueExpression = GetValueExpression(value);
+
+            return new WBooleanComparisonExpression()
+                    {
+                        ComparisonType = BooleanComparisonType.Equals,
+                        FirstExpr = GetColumnReferenceExpression(gremlinVar.VariableName, key),
+                        SecondExpr = ValueExpression
+                    };
+        }
+
+        internal static WBooleanComparisonExpression GetBooleanComparisonExpression(GremlinVariable gremlinVar,
+                                                                             string key, Predicate predicate)
+        {
+            WMultiPartIdentifier MultiIdentifierValue = GremlinUtil.GetMultiPartIdentifier(gremlinVar.VariableName, key);
+            WScalarExpression ValueExpression = GremlinUtil.GetValueExpression(predicate.Value);
+
+            return new WBooleanComparisonExpression()
+                    {
+                        ComparisonType = GremlinUtil.GetComparisonTypeFromPredicateType(predicate.PredicateType),
+                        FirstExpr = GremlinUtil.GetColumnReferenceExpression(gremlinVar.VariableName, key),
+                        SecondExpr = ValueExpression
+                    };
+        }
+
+        internal static WBooleanBinaryExpression GetBooleanBinaryExpression(WBooleanExpression booleanExpr1, WBooleanExpression booleanExpr2)
+        {
+            return new WBooleanBinaryExpression()
+            {
+                BooleanExpressionType = BooleanBinaryExpressionType.And,
+                FirstExpr = booleanExpr1,
+                SecondExpr = booleanExpr2
+            };
         }
     }
 }
--- GraphView/GremlinTranslationOps/filter/GremlinHasOp.cs
+++ GraphView/GremlinTranslationOps/filter/GremlinHasOp.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.SqlServer.TransactSql.ScriptDom;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,7 @@ namespace GraphView.GremlinTranslationOps.filter
     internal class GremlinHasOp: GremlinTranslationOperator
     {
         public string Key;
-        public string Value;
+        public object Value;
         public string Label;
         public Predicate Predicate;
         public GremlinTranslationOperator ParamOp;
@@ -24,14 +25,14 @@ namespace GraphView.GremlinTranslationOps.filter
             hasKeyTraversal
         }
 
-        public GremlinHasOp(string key, string value)
+        public GremlinHasOp(string key, object value)
         {
             Key = key;
             Value = value;
             OpType = HasOpType.hasKeyValue;
         }
 
-        public GremlinHasOp(string label, string key, string value)
+        public GremlinHasOp(string label, string key, object value)
         {
             Label = label;
             Key = key;
@@ -61,27 +62,38 @@ namespace GraphView.GremlinTranslationOps.filter
         public override GremlinToSqlContext GetContext()
         {
             GremlinToSqlContext inputContext = GetInputContext();
+            GremlinVariable currVar = inputContext.LastVariable;
 
             if (OpType == HasOpType.hasKey)
             {
                 //has(key)
-
+                WBooleanExpression booleanExpr = GremlinUtil.GetBooleanComparisonExpression(currVar, "type", Key);
+                inputContext.AddVariablePredicate(currVar, booleanExpr);
             }
             else if (OpType == HasOpType.hasKeyValue)
             {
                 //has(key, value)
+                WBooleanExpression booleanExpr = GremlinUtil.GetBooleanComparisonExpression(currVar, Key, Value);
+                inputContext.AddVariablePredicate(currVar, booleanExpr);
             }
             else if (OpType == HasOpType.hasKeyPredicate)
             {
                 //has(key, predicate)
+                WBooleanExpression booleanExpr = GremlinUtil.GetBooleanComparisonExpression(currVar, Key, Predicate);
+                inputContext.AddVariablePredicate(currVar, booleanExpr);
             }
             else if (OpType == HasOpType.hasLabelKeyValue)
             {
                 //has(label, key, value)
+                WBooleanExpression booleanExpr1 = GremlinUtil.GetBooleanComparisonExpression(currVar, "type", Label);
+                WBooleanExpression booleanExpr2 = GremlinUtil.GetBooleanComparisonExpression(currVar, Key, Value);
+                WBooleanExpression booleanExprBoth = GremlinUtil.GetBooleanBinaryExpression(booleanExpr1, booleanExpr2);
+                inputContext.AddVariablePredicate(currVar, booleanExprBoth);
             }
             else if (OpType == HasOpType.hasKeyTraversal)
             {
                 //has(key, traversal)
+                
             }
             return inputContext;
         }
