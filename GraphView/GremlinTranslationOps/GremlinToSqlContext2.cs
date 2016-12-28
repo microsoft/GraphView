e057453524ec54d15ffa42d0bbe1a7520e97d9e9
jeche <jeche@LJCHEN-DESKTOP>
2016-12-28 21:03:42 +0800

    Added more Gremlin variables for new translation.

--- GraphView/GremlinTranslationOps/GremlinToSqlContext2.cs
+++ GraphView/GremlinTranslationOps/GremlinToSqlContext2.cs
@@ -10,98 +10,51 @@ namespace GraphView
     {
         public GremlinVariable2 PivotVariable { get; set; }
         public Dictionary<string, GremlinVariable2> TaggedVariables;
-        public List<GremlinScalarVariable> ProjectedVariables;
-
         public List<GremlinVariable2> VariableList { get; private set; }
 
+
+        public List<ISqlTable> TableReferences { get; private set; }
+        public List<ISqlScalar> ProjectedVariables;
         public GremlinGroupVariable GroupVariable { get; set; }
 
         public GremlinToSqlContext2()
         {
             TaggedVariables = new Dictionary<string, GremlinVariable2>();
-            ProjectedVariables = new List<GremlinScalarVariable>();
+            ProjectedVariables = new List<ISqlScalar>();
             VariableList = new List<GremlinVariable2>();
         }
 
-        internal void As(string asName)
+        public GremlinToSqlContext2 Duplicate()
         {
-            TaggedVariables[asName] = PivotVariable;
-        }
-
-        internal void By(GremlinToSqlContext2 byContext)
-        {
-            // To consider: By steps may be applied to variables in order
-            // PivotVariable.By(this, byContext);
-            if (VariableList.Count > 0)
+            return new GremlinToSqlContext2()
             {
-                VariableList[VariableList.Count - 1].By(this, byContext);
-            }
+                VariableList = new List<GremlinVariable2>(this.VariableList),
+                TaggedVariables = new Dictionary<string, GremlinVariable2>(this.TaggedVariables),
+                PivotVariable = this.PivotVariable,
+                TableReferences = new List<ISqlTable>(this.TableReferences),
+                ProjectedVariables = new List<ISqlScalar>(ProjectedVariables),
+                GroupVariable = GroupVariable   // more properties need to be added when GremlinToSqlContext2 is changed.
+            };
         }
 
-        internal void By(string byName)
+        public void Reset()
         {
-            // To consider: BY steps may be applied to variables in order
-            // PivotVariable.By(this, byName);
-            if (VariableList.Count > 0)
-            {
-                VariableList[VariableList.Count - 1].By(this, byName);
-            }
-        }
+            PivotVariable = null;
+            TaggedVariables.Clear();
+            VariableList.Clear();
 
-        internal void Cap(params string[] keys)
-        {
+            TableReferences.Clear();
             ProjectedVariables.Clear();
+            GroupVariable = null;
 
-            foreach (string key in keys)
-            {
-                if (!TaggedVariables.ContainsKey(key))
-                {
-                    throw new QueryCompilationException(string.Format("The specified tag \"{0}\" cannot be found.", key));
-                }
-            }
+            // More resetting goes here when more properties are added to GremlinToSqlContext2
         }
 
         internal void Coalesce(
             GremlinToSqlContext2 traversal1,
             GremlinToSqlContext2 traversal2)
         {
-            GremlinVariableType type1 = traversal1.ProjectedVariables.Count == 1 ?
-                traversal1.ProjectedVariables[0].GetVariableType() : GremlinVariableType.Table;
-            GremlinVariableType type2 = traversal2.ProjectedVariables.Count == 1 ?
-                traversal2.ProjectedVariables[1].GetVariableType() : GremlinVariableType.Table;
-
-            if (type1 == type2)
-            {
-                switch (type1)
-                {
-                    case GremlinVariableType.Vertex:
-                        GremlinCoalesceVertexVariable vertexVariable = new GremlinCoalesceVertexVariable(traversal1, traversal2);
-                        VariableList.Add(vertexVariable);
-                        PivotVariable = vertexVariable;
-                        break;
-                    case GremlinVariableType.Edge:
-                        GremlinCoalesceEdgeVariable edgeVariable = new GremlinCoalesceEdgeVariable(traversal1, traversal2);
-                        VariableList.Add(edgeVariable);
-                        PivotVariable = edgeVariable;
-                        break;
-                    case GremlinVariableType.Table:
-                        GremlinCoalesceTableVariable tabledValue = new GremlinCoalesceTableVariable(traversal1, traversal2);
-                        VariableList.Add(tabledValue);
-                        PivotVariable = tabledValue;
-                        break;
-                    case GremlinVariableType.Scalar:
-                        PivotVariable = new GremlinCoalesceValueVariable(traversal1, traversal2);
-                        break;
-                    default:
-                        break;
-                }
-            }
-            else
-            {
-                GremlinCoalesceTableVariable tabledValue = new GremlinCoalesceTableVariable(traversal1, traversal2);
-                VariableList.Add(tabledValue);
-                PivotVariable = tabledValue;
-            }
+            
         }
 
         internal void Populate(string propertyName)
@@ -114,18 +67,6 @@ namespace GraphView
             }
 
             PivotVariable.Populate(propertyName);
-            ProjectedVariables.Add(new GremlinVariableProperty(PivotVariable, propertyName));
-        }
-
-        internal void Group()
-        {
-            GroupVariable = new GremlinGroupVariable();
-            VariableList.Add(GroupVariable);
-        }
-
-        internal void Both()
-        {
-
         }
     }
 
--- GraphView/GremlinTranslationOps/GremlinVariable2.cs
+++ GraphView/GremlinTranslationOps/GremlinVariable2.cs
@@ -6,12 +6,20 @@ using System.Threading.Tasks;
 
 namespace GraphView
 {
+    internal interface ISqlTable
+    {
+    }
+
+    internal interface ISqlScalar { }
+
+    internal interface ISqlBoolean { }
+
     internal enum GremlinVariableType
     {
         Vertex,
         Edge,
-        Scalar,
         Table,
+        Scalar,
         Undefined
     }
 
@@ -26,11 +34,21 @@ namespace GraphView
 
         internal virtual void Populate(string name) { }
 
+        internal virtual GremlinScalarVariable DefaultProjection()
+        {
+            throw new NotImplementedException();
+        }
+
         internal virtual void And(
             GremlinToSqlContext2 currentContext, 
             GremlinToSqlContext2 subContext1,
             GremlinToSqlContext2 subContext2)
         {
+        }
+
+        internal virtual void As(GremlinToSqlContext2 currentContext, string name)
+        {
+            currentContext.TaggedVariables[name] = this;
         }
 
         internal virtual void Both(GremlinToSqlContext2 currentContext)
@@ -46,13 +64,106 @@ namespace GraphView
         {
         }
 
+        internal virtual void Cap(GremlinToSqlContext2 currentContext, params string[] keys)
+        {
+            currentContext.ProjectedVariables.Clear();
+
+            foreach (string key in keys)
+            {
+                if (!currentContext.TaggedVariables.ContainsKey(key))
+                {
+                    throw new QueryCompilationException(string.Format("The specified tag \"{0}\" is not defined.", key));
+                }
+
+                GremlinVariable2 var = currentContext.TaggedVariables[key];
+                currentContext.ProjectedVariables.Add(var.DefaultProjection());
+            }
+        }
+
+        internal virtual void Coalesce(
+            GremlinToSqlContext2 currentContext,
+            GremlinToSqlContext2 traversal1,
+            GremlinToSqlContext2 traversal2)
+        {
+            GremlinVariableType type1 = traversal1.PivotVariable.GetVariableType();
+            GremlinVariableType type2 = traversal2.PivotVariable.GetVariableType();
+
+            if (type1 == type2)
+            {
+                switch (type1)
+                {
+                    case GremlinVariableType.Vertex:
+                        GremlinCoalesceVertexVariable vertexVariable = new GremlinCoalesceVertexVariable(traversal1, traversal2);
+                        currentContext.VariableList.Add(vertexVariable);
+                        currentContext.TableReferences.Add(vertexVariable);
+                        currentContext.PivotVariable = vertexVariable;
+                        break;
+                    case GremlinVariableType.Edge:
+                        GremlinCoalesceEdgeVariable edgeVariable = new GremlinCoalesceEdgeVariable(traversal1, traversal2);
+                        currentContext.VariableList.Add(edgeVariable);
+                        currentContext.TableReferences.Add(edgeVariable);
+                        currentContext.PivotVariable = edgeVariable;
+                        break;
+                    case GremlinVariableType.Table:
+                        GremlinCoalesceTableVariable tabledValue = new GremlinCoalesceTableVariable(traversal1, traversal2);
+                        currentContext.VariableList.Add(tabledValue);
+                        currentContext.TableReferences.Add(tabledValue);
+                        currentContext.PivotVariable = tabledValue;
+                        break;
+                    case GremlinVariableType.Scalar:
+                        currentContext.PivotVariable = new GremlinCoalesceValueVariable(traversal1, traversal2);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                GremlinCoalesceTableVariable tabledValue = new GremlinCoalesceTableVariable(traversal1, traversal2);
+                currentContext.VariableList.Add(tabledValue);
+                currentContext.TableReferences.Add(tabledValue);
+                currentContext.PivotVariable = tabledValue;
+            }
+        }
+
         internal virtual void Count()
         {
 
         }
+
+        internal virtual void Group(GremlinToSqlContext2 currentContext)
+        {
+            GremlinGroupVariable groupVariable = new GremlinGroupVariable();
+            currentContext.VariableList.Add(groupVariable);
+        }
+
+        internal virtual void Inject(GremlinToSqlContext2 currentContext, params string[] values)
+        {
+            if (currentContext.VariableList.Count == 0)
+            {
+                GremlinInjectVariable injectVar = new GremlinInjectVariable(null, values);
+                currentContext.VariableList.Add(injectVar);
+                currentContext.PivotVariable = injectVar;
+            } 
+            else
+            {
+                GremlinToSqlContext2 priorContext = currentContext.Duplicate();
+                currentContext.Reset();
+                GremlinInjectVariable injectVar = new GremlinInjectVariable(priorContext, values);
+                currentContext.VariableList.Add(injectVar);
+                currentContext.PivotVariable = injectVar;
+            }
+        }
     }
 
-    internal abstract class GremlinScalarVariable : GremlinVariable2 { }
+    
+    internal abstract class GremlinScalarVariable : GremlinVariable2, ISqlScalar
+    {
+        internal override GremlinScalarVariable DefaultProjection()
+        {
+            return this;
+        }
+    }
 
     internal class GremlinVariableProperty : GremlinScalarVariable
     {
@@ -64,6 +175,7 @@ namespace GraphView
             GremlinVariable = gremlinVariable;
             VariableProperty = variableProperty;
         }
+
     }
 
     internal class GremlinScalarSubquery : GremlinScalarVariable
@@ -75,19 +187,22 @@ namespace GraphView
             SubqueryContext = SubqueryContext;
         }
     }
-    
-    internal class GremlinFreeVertexVariable : GremlinVariable2
+
+    internal abstract class GremlinTableVariable : GremlinVariable2
     {
-        private static long _count = 0;
+        protected static int _count = 0;
 
-        public static string GetVariableName()
+        public string GenerateTableAlias()
         {
-            return "N_" + _count++;
+            return "R_" + _count++;
         }
-
+    }
+    
+    internal class GremlinFreeVertexVariable : GremlinTableVariable, ISqlTable
+    {
         public GremlinFreeVertexVariable()
         {
-            VariableName = GetVariableName();
+            VariableName = GenerateTableAlias();
         }
 
         internal override void Both(GremlinToSqlContext2 currentContext)
@@ -104,20 +219,26 @@ namespace GraphView
     /// the FROM clause, whereas a bound vertex variable is translated into
     /// a table-valued function. 
     /// </summary>
-    internal class GremlinBoundVertexVariable : GremlinVariable2
+    internal class GremlinBoundVertexVariable : GremlinTableVariable, ISqlTable
     {
-        private static long _count = 0;
         private GremlinVariableProperty adjacencyList;
-
-        public static string GetVariableName()
-        {
-            return "BN_" + _count++;
-        }
+        private List<string> projectedProperties;
 
         public GremlinBoundVertexVariable(GremlinVariableProperty adjacencyList)
         {
-            VariableName = GetVariableName();
+            VariableName = GenerateTableAlias();
             this.adjacencyList = adjacencyList;
+            projectedProperties = new List<string>();
+        }
+
+        internal override GremlinScalarVariable DefaultProjection()
+        {
+            return new GremlinVariableProperty(this, "id");
+        }
+
+        internal override void Populate(string name)
+        {
+            projectedProperties.Add(name);
         }
 
         internal override void Both(GremlinToSqlContext2 currentContext)
@@ -129,20 +250,18 @@ namespace GraphView
         }
     }
 
-    internal abstract class GremlinCoalesceVariable : GremlinVariable2
+    /// <summary>
+    /// The abstract variable for coalesce and union 
+    /// </summary>
+    internal abstract class GremlinBinaryVariable : GremlinTableVariable
     {
         protected GremlinToSqlContext2 traversal1;
         protected GremlinToSqlContext2 traversal2;
 
-        public GremlinCoalesceVariable(GremlinToSqlContext2 traversal1, GremlinToSqlContext2 traversal2)
+        public GremlinBinaryVariable(GremlinToSqlContext2 traversal1, GremlinToSqlContext2 traversal2)
         {
             this.traversal1 = traversal1;
             this.traversal2 = traversal2;
-        }
-
-        public override GremlinVariableType GetVariableType()
-        {
-            return GremlinVariableType.Table;
         }
 
         internal override void Populate(string name)
@@ -182,7 +301,20 @@ namespace GraphView
         }
     }
 
-    internal class GremlinCoalesceVertexVariable : GremlinCoalesceVariable
+    
+    internal abstract class GremlinCoalesceVariable : GremlinBinaryVariable
+    {
+        
+        public GremlinCoalesceVariable(GremlinToSqlContext2 traversal1, GremlinToSqlContext2 traversal2)
+            : base(traversal1, traversal2) { }
+
+        public override GremlinVariableType GetVariableType()
+        {
+            return GremlinVariableType.Table;
+        }
+    }
+
+    internal class GremlinCoalesceVertexVariable : GremlinCoalesceVariable, ISqlTable
     {
         public GremlinCoalesceVertexVariable(GremlinToSqlContext2 traversal1, GremlinToSqlContext2 traversal2)
             : base (traversal1, traversal2) { }
@@ -192,13 +324,18 @@ namespace GraphView
             return GremlinVariableType.Vertex;
         }
 
+        internal override GremlinScalarVariable DefaultProjection()
+        {
+            return new GremlinVariableProperty(this, "id");
+        }
+
         internal override void Both(GremlinToSqlContext2 currentContext)
         {
             base.Both(currentContext);
         }
     }
 
-    internal class GremlinCoalesceEdgeVariable : GremlinCoalesceVariable
+    internal class GremlinCoalesceEdgeVariable : GremlinCoalesceVariable, ISqlTable
     {
         public GremlinCoalesceEdgeVariable(GremlinToSqlContext2 traversal1, GremlinToSqlContext2 traversal2)
             : base (traversal1, traversal2) { }
@@ -209,7 +346,7 @@ namespace GraphView
         }
     }
 
-    internal class GremlinCoalesceTableVariable : GremlinCoalesceVariable
+    internal class GremlinCoalesceTableVariable : GremlinCoalesceVariable, ISqlTable
     {
         public GremlinCoalesceTableVariable(GremlinToSqlContext2 traversal1, GremlinToSqlContext2 traversal2)
             : base (traversal1, traversal2) { }
@@ -270,11 +407,81 @@ namespace GraphView
         }
     }
 
-    internal class GremlinCountVariable : GremlinVariable2
+    internal abstract class GremlinAggregationVariable : GremlinVariable2, ISqlScalar
+    {
+
+    }
+
+    internal class GremlinCountVariable : GremlinAggregationVariable
     {
         public override GremlinVariableType GetVariableType()
         {
             return GremlinVariableType.Scalar;
         }
+    }
+
+    internal class GremlinFoldVariable : GremlinAggregationVariable
+    {
+
+    }
+
+    internal class GremlinUnfoldVariable : GremlinTableVariable, ISqlTable
+    {
+
+    }
+
+    /// <summary>
+    /// Inject variable will be translated to a derived table reference
+    /// in the SQL FROM clause, concatenating results from priorContext and injected values. 
+    /// </summary>
+    internal class GremlinInjectVariable : GremlinTableVariable, ISqlTable
+    {
+        List<string> rows;
+        // When priorContext is null, the corresponding table reference only contains injected values. 
+        GremlinToSqlContext2 priorContext;
+
+        public GremlinInjectVariable(GremlinToSqlContext2 priorContext, params string[] values)
+        {
+            this.priorContext = priorContext;
+            rows = new List<string>(values);
+        }
+
+        public override GremlinVariableType GetVariableType()
+        {
+            return GremlinVariableType.Table;
+        }
+
+        internal override GremlinScalarVariable DefaultProjection()
+        {
+            // When priorContext is not null, the output table has one column,
+            // and the column name is determined by priorContext.
+            if (priorContext != null)
+            {
+                return priorContext.PivotVariable.DefaultProjection();
+            }
+            else
+            {
+                VariableName = GenerateTableAlias();
+                return new GremlinVariableProperty(this, "_value");
+            }
+        }
+
+        internal override void Populate(string name)
+        {
+            if (priorContext != null)
+            {
+                priorContext.Populate(name);
+            }
+        }
+
+        internal override void Inject(GremlinToSqlContext2 currentContext, params string[] values)
+        {
+            rows.AddRange(values);
+        }
+    }
+
+    internal class GremlinOptionalVariable : GremlinVariable2, ISqlTable
+    {
+
     }
 }
