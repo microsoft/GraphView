fdbf2ce572b96d60bb296ee5f31ce182ad8b5319
linjinjin123 <494779656@qq.com>
2017-02-15 10:31:50 +0800

    remove log

--- GraphView/GremlinTranslation2/GraphTraversal2.cs
+++ GraphView/GremlinTranslation2/GraphTraversal2.cs
@@ -116,8 +116,8 @@ namespace GraphView
         {
             var sqlScript = LastGremlinTranslationOp.ToSqlScript();
             var str = sqlScript.ToString();
-            Console.WriteLine(str);     // Added temporarily for debugging purpose.
-            Console.WriteLine();
+            //Console.WriteLine(str);     // Added temporarily for debugging purpose.
+            //Console.WriteLine();
 
             var op = sqlScript.Batches[0].Compile(null, Connection);
             var rawRecordResults = new List<RawRecord>();
