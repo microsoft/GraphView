b74d9ec65524cddf94491ef206871d12b1af0fe9
linjinjin123 <494779656@qq.com>
2017-03-05 23:10:36 +0800

    fix range

--- GraphView/GremlinTranslation/variables/table/tvf/singleType/GremlinRangeVariable.cs
+++ GraphView/GremlinTranslation/variables/table/tvf/singleType/GremlinRangeVariable.cs
@@ -30,7 +30,7 @@ namespace GraphView
             parameters.Add(SqlUtil.GetValueExpr(Low));
             parameters.Add(SqlUtil.GetValueExpr(High));
             parameters.Add(IsLocal ? SqlUtil.GetValueExpr(1) : SqlUtil.GetValueExpr(-1));
-            parameters.Add(IsReverse ? SqlUtil.GetValueExpr(-1): SqlUtil.GetValueExpr(1));
+            parameters.Add(IsReverse ? SqlUtil.GetValueExpr(1): SqlUtil.GetValueExpr(-1));
             var secondTableRef = SqlUtil.GetFunctionTableReference(GremlinKeyword.func.Range, parameters, this, GetVariableName());
             return SqlUtil.GetCrossApplyTableReference(null, secondTableRef);
         }
