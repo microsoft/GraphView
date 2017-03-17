ab3dba315ae4bfb152afeff1e737bc4bc56d47eb
yanjga <yanjga@microsoft.com>
2017-03-17 14:03:41 +0800

    Fix shortest path bug and add test case

--- GraphViewUnitTest/ShortestPathTest.cs
+++ GraphViewUnitTest/ShortestPathTest.cs
@@ -14,68 +14,80 @@ namespace GraphViewUnitTest
     public class ShortestPathTest
     {
         [TestMethod]
-        public void GraphViewCreateMarvelDataSet()
+        public void LoadClassicGraphData()
         {
             GraphViewConnection connection = new GraphViewConnection("https://graphview.documents.azure.com:443/",
-                "MqQnw4xFu7zEiPSD+4lLKRBQEaQHZcKsjlHxXn2b96pE/XlJ8oePGhjnOofj1eLpUdsfYgEhzhejk2rjH/+EKA==",
-                "GroupMatch", "MarvelTest");
+              "MqQnw4xFu7zEiPSD+4lLKRBQEaQHZcKsjlHxXn2b96pE/XlJ8oePGhjnOofj1eLpUdsfYgEhzhejk2rjH/+EKA==",
+              "GroupMatch", "MarvelTest");
             connection.ResetCollection();
-            GraphViewCommand cmd = new GraphViewCommand(connection);
+            GraphViewCommand graphCommand = new GraphViewCommand(connection);
 
-            cmd.CommandText = "g.addV('character').property('name', 'VENUS II').property('weapon', 'shield').next()";
-            cmd.Execute();
-            cmd.CommandText = "g.addV('comicbook').property('name', 'AVF 4').next()";
-            cmd.Execute();
-            cmd.CommandText = "g.V().has('name', 'VENUS II').addE('appeared').to(g.V().has('name', 'AVF 4')).next()";
-            cmd.Execute();
-            cmd.CommandText = "g.addV('character').property('name', 'HAWK').property('weapon', 'claws').next()";
-            cmd.Execute();
-            cmd.CommandText = "g.V().as('v').has('name', 'HAWK').addE('appeared').to(g.V().has('name', 'AVF 4')).next()";
-            cmd.Execute();
-            cmd.CommandText = "g.addV('character').property('name', 'WOODGOD').property('weapon', 'lasso').next()";
-            cmd.Execute();
-            cmd.CommandText = "g.V().as('v').has('name', 'WOODGOD').addE('appeared').to(g.V().has('name', 'AVF 4')).next()";
-            cmd.Execute();
+            graphCommand.g().AddV("person").Property("name", "marko").Property("age", 29).Next();
+            graphCommand.g().AddV("person").Property("name", "vadas").Property("age", 27).Next();
+            graphCommand.g().AddV("software").Property("name", "lop").Property("lang", "java").Next();
+            graphCommand.g().AddV("person").Property("name", "josh").Property("age", 32).Next();
+            graphCommand.g().AddV("software").Property("name", "ripple").Property("lang", "java").Next();
+            graphCommand.g().AddV("person").Property("name", "peter").Property("age", 35).Next();
+            graphCommand.g().V().Has("name", "marko").AddE("knows").Property("weight", 0.5d).To(graphCommand.g().V().Has("name", "vadas")).Next();
+            graphCommand.g().V().Has("name", "marko").AddE("knows").Property("weight", 1.0d).To(graphCommand.g().V().Has("name", "josh")).Next();
+            graphCommand.g().V().Has("name", "marko").AddE("created").Property("weight", 0.4d).To(graphCommand.g().V().Has("name", "lop")).Next();
+            graphCommand.g().V().Has("name", "josh").AddE("created").Property("weight", 1.0d).To(graphCommand.g().V().Has("name", "ripple")).Next();
+            graphCommand.g().V().Has("name", "josh").AddE("created").Property("weight", 0.4d).To(graphCommand.g().V().Has("name", "lop")).Next();
+            graphCommand.g().V().Has("name", "peter").AddE("created").Property("weight", 0.2d).To(graphCommand.g().V().Has("name", "lop")).Next();
+
+            graphCommand.Dispose();
+            connection.Dispose();
         }
+
         [TestMethod]
         public void ShortestPath()
-        {
-            String src = "3b93929f-6ec7-4af1-b62c-89f0c46cc1da";
-            String des = "7974ea88-e418-4803-9b75-6ad6d83f775e";
-            GetShortestPath(src, des);
-        }
-        [TestMethod]
-        public void GetShortestPath(String src, String des)
         {
             GraphViewConnection connection = new GraphViewConnection("https://graphview.documents.azure.com:443/",
               "MqQnw4xFu7zEiPSD+4lLKRBQEaQHZcKsjlHxXn2b96pE/XlJ8oePGhjnOofj1eLpUdsfYgEhzhejk2rjH/+EKA==",
               "GroupMatch", "MarvelTest");
             GraphViewCommand graph = new GraphViewCommand(connection);
-            graph.OutputFormat = OutputFormat.GraphSON;
+
+            var src1 = graph.g().V().HasLabel("person").Has("name", Predicate.eq("marko")).Values("id").Next()[0];
+            var des1 = graph.g().V().HasLabel("software").Has("name", Predicate.eq("lop")).Values("id").Next()[0];
+            int result1 = GetShortestPath(src1, des1, graph);
+            Assert.AreEqual(result1, 1);
+
+            var src2 = graph.g().V().HasLabel("person").Has("name", Predicate.eq("marko")).Values("id").Next()[0];
+            var des2 = graph.g().V().HasLabel("software").Has("name", Predicate.eq("ripple")).Values("id").Next()[0];
+            var result2 = GetShortestPath(src2, des2, graph);
+            Assert.AreEqual(result2, 2);
+
+            var src3 = graph.g().V().HasLabel("person").Has("name", Predicate.eq("peter")).Values("id").Next()[0];
+            var des3 = graph.g().V().HasLabel("software").Has("name", Predicate.eq("lop")).Values("id").Next()[0];
+            var result3 = GetShortestPath(src3, des3, graph);
+            Assert.AreEqual(result3, 1);
+        }
+        public int GetShortestPath(String src, String des, GraphViewCommand graph)
+        {
             Queue<String> vertexIdQ1 = new Queue<String>();
             Queue<String> vertexIdQ2 = new Queue<String>();
             Boolean reachDes = false;
-            int depth = 0;
+            int depth = 1;
             vertexIdQ1.Enqueue(src);
 
             while(!reachDes && vertexIdQ1.Count != 0)
             {
                 var id = vertexIdQ1.Dequeue();
-                var tempVertexIds = graph.g().V().Has("id", id).Values("id").Next();
-                foreach(var vertexId in tempVertexIds)
+                var tempVertexIds = graph.g().V().HasId(id).Out().Values("id").Next();
+
+                foreach (var vertexId in tempVertexIds)
                 {
                     if(vertexId == des)
                     {
                         reachDes = true;
-                        depth ++;
                         break;
-                    } else
+                    } else if(vertexId != src)
                     {
                         vertexIdQ2.Enqueue(vertexId);
                     }
                 }
                 // the uppper level queue become empty, move to next level of graph
-                if(vertexIdQ1.Count == 0)
+                if(vertexIdQ1.Count == 0 && !reachDes)
                 {
                     var swap = vertexIdQ1;
                     vertexIdQ1 = vertexIdQ2;
@@ -86,11 +98,12 @@ namespace GraphViewUnitTest
 
             if (reachDes)
             {
-                Console.WriteLine("Shortest Path from {0} to {1} is {2}", src, des, depth);
+                Console.WriteLine("Shortest Path from {0} to {1}, depth is {2}", src, des, depth);
             } else
             {
-                Console.WriteLine("No path from {0} to {1} is {2}", src, des, depth);
+                Console.WriteLine("No path from {0} to {1}, depth is {2}", src, des, 0);
             }
+            return depth;
         }
     }
 }
