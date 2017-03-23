335bba17404c07eac28cfe5ada1fc979c52634da
Yanjie Gao <yanjga@microsoft.com>
2017-03-23 11:40:15 +0800

    Add FlatMapTest and FoldTest

---
+++ GraphViewUnitTest/Gremlin/ProcessTests/Traversal/Step/Map/FlatMapTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphView;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GraphViewUnitTest.Gremlin.ProcessTests.Traversal.Step.Map
+{
+    [TestClass]
+    public class FlatMapTest : AbstractGremlinTest
+    {
+        /// <summary>
+        /// g_V_asXaX_flatMapXselectXaXX()
+        /// from org/apache/tinkerpop/gremlin/process/traversal/step/map/FlatMapTest.java
+        /// Gremlin: g.V().as("a").flatMap(select("a"));
+        /// </summary>
+        [TestMethod]
+        public void FlatMapWithSelect()
+        {
+            using (GraphViewCommand graphCommand = new GraphViewCommand(graphConnection))
+            {
+                var traversal = graphCommand.g().V()
+                    .As("a")
+                    .FlatMap(GraphTraversal2.__().Select("a"));
+                var result = traversal.Next();
+                Assert.AreEqual(6, result.Count);
+            }
+        }
+    }
+}
---
+++ GraphViewUnitTest/Gremlin/ProcessTests/Traversal/Step/Map/FoldTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphView;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GraphViewUnitTest.Gremlin.ProcessTests.Traversal.Step.Map
+{
+    [TestClass]
+    public class FoldTest : AbstractGremlinTest
+    {
+        /// <summary>
+        /// g_V_fold()
+        /// from org/apache/tinkerpop/gremlin/process/traversal/step/map/FoldTest.java
+        /// Gremlin: g.V().fold();
+        /// </summary>
+        [TestMethod]
+        public void BasicFold()
+        {
+            using (GraphViewCommand graphCommand = new GraphViewCommand(graphConnection))
+            {
+                graphCommand.OutputFormat = OutputFormat.GraphSON;
+                var traversal = graphCommand.g().V().Fold();
+                dynamic result = JsonConvert.DeserializeObject<dynamic>(traversal.Next().FirstOrDefault());
+                graphCommand.OutputFormat = OutputFormat.Regular;
+
+                Assert.AreEqual(1, ((JArray)result).Count);
+                Assert.AreEqual(6, ((JArray)result[0]).Count);
+            }
+        }
+
+        /// <summary>
+        /// g_V_fold_unfold()
+        /// from org/apache/tinkerpop/gremlin/process/traversal/step/map/FoldTest.java
+        /// Gremlin: g.V().fold().unfold();
+        /// </summary>
+        [TestMethod]
+        public void FoldThenUnfold()
+        {
+            using (GraphViewCommand graphCommand = new GraphViewCommand(graphConnection))
+            {
+                graphCommand.OutputFormat = OutputFormat.GraphSON;
+                var traversal = graphCommand.g().V().Fold().Unfold();
+                dynamic result = JsonConvert.DeserializeObject<dynamic>(traversal.Next().FirstOrDefault());
+                graphCommand.OutputFormat = OutputFormat.Regular;
+
+                Assert.AreEqual(6, ((JArray)result).Count);
+            }
+        }
+    }
+}
