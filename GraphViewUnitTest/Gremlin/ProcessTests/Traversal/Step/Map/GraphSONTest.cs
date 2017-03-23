007f6171caeba21ad5a3954cf793ba8fad0cafad
Yanjie Gao <yanjga@microsoft.com>
2017-03-23 11:59:12 +0800

    Add GraphSONTest

---
+++ GraphViewUnitTest/Gremlin/ProcessTests/Traversal/Step/Map/GraphSONTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphView;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+namespace GraphViewUnitTest.Gremlin.ProcessTests.Traversal.Step.Map
+{
+    [TestClass]
+    public class GraphSONTest : AbstractGremlinTest
+    {
+        /// <summary>
+        /// Creates a vertex with properties
+        /// </summary>
+        [TestMethod]
+        public async Task AddVertexTest()
+        {
+            GraphViewCommand cmd = new GraphViewCommand(graphConnection);
+            cmd.OutputFormat = OutputFormat.GraphSON;
+            cmd.CommandText = "g.AddV('character').Property('name', 'VENUS II').Property('weapon', 'shield')";
+            cmd.OutputFormat = OutputFormat.GraphSON;
+            var results = cmd.Execute();
+
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+
+            graphConnection.ResetCollection();
+        }
+    }
+}
