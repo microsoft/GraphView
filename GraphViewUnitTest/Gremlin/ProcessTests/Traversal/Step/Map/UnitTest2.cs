aa25ca3b45bea8e35c2de55cf0e8a973041b2409
Yanjie Gao <yanjga@microsoft.com>
2017-03-24 15:26:10 +0800

    Add LocalTest

--- GraphViewUnitTest/Gremlin/ProcessTests/Traversal/Step/Map/UnitTest2.cs
+++ GraphViewUnitTest/Gremlin/ProcessTests/Traversal/Step/Map/UnitTest2.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace GraphViewUnitTest.Gremlin.ProcessTests.Traversal.Step.Map
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest2
     {
         [TestMethod]
         public void TestMethod1()
