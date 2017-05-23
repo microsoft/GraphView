ea913b02b322030885bd0faf13bfc564d3e09ce2
Wenhao Huang <v-wenhh@microsoft.com>
2017-05-23 15:45:02 +0800

    the ContextVariable ProjectedProperties should be the subset of the RealVariable one

--- GraphView/GremlinTranslation/variables/GremlinContextVariable.cs
+++ GraphView/GremlinTranslation/variables/GremlinContextVariable.cs
@@ -33,8 +33,25 @@ namespace GraphView
 
         internal override void Populate(string property)
         {
-            base.Populate(property);
             RealVariable.Populate(property);
+
+            switch (RealVariable.GetVariableType())
+            {
+                case GremlinVariableType.Vertex:
+                    if (GremlinUtil.IsEdgeProperty(property)) return;
+                    break;
+                case GremlinVariableType.Edge:
+                    if (GremlinUtil.IsVertexProperty(property)) return;
+                    break;
+                case GremlinVariableType.VertexProperty:
+                    if (GremlinUtil.IsVertexProperty(property) || GremlinUtil.IsEdgeProperty(property)) return;
+                    break;
+                case GremlinVariableType.Scalar:
+                case GremlinVariableType.Property:
+                    if (property != GremlinKeyword.TableDefaultColumnName) return;
+                    break;
+            }
+            base.Populate(property);
         }
 
         internal override void PopulateStepProperty(string property)
