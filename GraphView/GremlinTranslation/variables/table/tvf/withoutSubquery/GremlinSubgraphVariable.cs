72a38cfa71574832c2f0b63316b78d3d5f668016
Wenhao Huang <v-wenhh@microsoft.com>
2017-07-12 15:36:29 +0800

    [Unfinished] add two Subgraph related files

---
+++ GraphView/GremlinTranslation/variables/table/tvf/withoutSubquery/GremlinSubgraphVariable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphView
+{
+    internal class GremlinSubgraphVariable : GremlinScalarTableVariable
+    {
+        public string SideEffectKey { get; set; }
+        public GremlinToSqlContext DummyContext { get; set; }
+
+        public GremlinSubgraphVariable(GremlinToSqlContext dummyContext, string sideEffectKey)
+        {
+            this.SideEffectKey = sideEffectKey;
+            this.DummyContext = dummyContext;
+            Labels.Add(sideEffectKey);
+        }
+
+        public override WTableReference ToTableReference()
+        {
+            List<WScalarExpression> parameters = new List<WScalarExpression>();
+            WSelectQueryBlock selectQueryBlock = DummyContext.ToSelectQueryBlock(true);
+            parameters.Add(SqlUtil.GetScalarSubquery(selectQueryBlock));
+            parameters.Add(SqlUtil.GetValueExpr(SideEffectKey));
+            var tableRef = SqlUtil.GetFunctionTableReference(GremlinKeyword.func.Subgraph, parameters, GetVariableName());
+            return SqlUtil.GetCrossApplyTableReference(tableRef);
+        }
+    }
+}
