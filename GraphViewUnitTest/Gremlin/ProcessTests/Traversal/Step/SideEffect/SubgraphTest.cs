b1cea84d218507f85323e74eabc3eb94106cf13c
Wenhao Huang <v-wenhh@microsoft.com>
2017-07-17 17:45:31 +0800

    [Update] add subgraph step test

---
+++ GraphViewUnitTest/Gremlin/ProcessTests/Traversal/Step/SideEffect/SubgraphTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using GraphView;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GraphViewUnitTest.Gremlin
+{
+    [TestClass]
+    public class SubgraphTest: AbstractGremlinTest
+    {
+        [TestMethod]
+        public void SubgraphSameAsOriginalGraph()
+        {
+            using (GraphViewCommand command = new GraphViewCommand(graphConnection))
+            {
+                command.OutputFormat = OutputFormat.GraphSON;
+                var originGraphSON = command.g().V().Next()[0];
+                command.OutputFormat = OutputFormat.Regular;
+                var subGraphSON = command.g().E().Subgraph("sg").Cap("sg").Next()[0];
+
+                var originGraph = JToken.Parse(originGraphSON);
+                var subGraph = JToken.Parse(subGraphSON);
+
+                Assert.IsTrue(JToken.DeepEquals(originGraph, subGraph));
+            }
+        }
+
+        [TestMethod]
+        public void SubgraphWithMultiVp()
+        {
+            using (GraphViewCommand command = new GraphViewCommand(graphConnection))
+            {
+                command.g()
+                    .V()
+                    .Has("name", "peter")
+                    .Property(GremlinKeyword.PropertyCardinality.List, "age", 13)
+                    .Property("qqq", false)
+                    .Next();
+
+                var traversal = command.g().E().Subgraph("sg").Cap("sg");
+                traversal.Next();
+            }
+        }
+        
+        [TestMethod]
+        public void SubgraphAggregation()
+        {
+            using (GraphViewCommand command = new GraphViewCommand(graphConnection))
+            {
+                
+                command.OutputFormat = OutputFormat.GraphSON;
+                var originGraphSON = command.g().V().Next()[0];
+                command.OutputFormat = OutputFormat.Regular;
+                command.CommandText =
+                    "g.E().hasLabel('created').subgraph('sg').bothV().bothE().subgraph('sg').cap('sg')";
+                var subGraphSON = command.ExecuteAndGetResults()[0];
+
+                var originGraph = JToken.Parse(originGraphSON);
+                var subGraph = JToken.Parse(subGraphSON);
+
+                Assert.IsTrue(JToken.DeepEquals(originGraph, subGraph));
+            }
+        }
+
+        [TestMethod]
+        public void SubgraphInRepeat()
+        {
+            using (GraphViewCommand command = new GraphViewCommand(graphConnection))
+            {
+                command.CommandText =
+                    "g.V().has('name', 'lop').repeat(__.inE().subgraph('subGraph').outV()).times(3).cap('subGraph')";
+                var subGraphSON = command.ExecuteAndGetResults()[0];
+                var subGraph = JArray.Parse(subGraphSON);
+
+                Assert.Equals(subGraph.Count, 4);
+            }
+        }
+    }
+}
