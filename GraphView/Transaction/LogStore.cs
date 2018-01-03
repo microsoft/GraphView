42f095d0375afff146132afa681a98f9921f6f31
jeche <jeche@LJCHEN-DESKTOP>
2018-01-03 18:45:43 +0800

    Add the skeleton of the transaction component.

---
+++ GraphView/Transaction/LogStore.cs
@@ -0,0 +1,81 @@
+﻿
+
+namespace GraphView.Transaction
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.Azure.Documents.Client;
+    using Newtonsoft.Json.Linq;
+
+    /// <summary>
+    /// A data store for logging
+    /// </summary>
+    public abstract class LogStore
+    {
+        public virtual long GetMaxTxSequenceNumber()
+        {
+            return 0;
+        }
+
+        public virtual IEnumerable<JObject> ReadJson(string recordId)
+        {
+            throw new NotImplementedException(); 
+        }
+
+        public virtual IEnumerable<JObject> ReadJson(IEnumerable<string> ridList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual IEnumerable<JObject> ReadJson(RecordQuery recordQuery)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void WriteJson(JObject record)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void WriteJson(IEnumerable<JObject> recordList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    public class CosmosDBStore : LogStore
+    {
+        private readonly string url;
+        private readonly string primaryKey;
+        private readonly string databaseId;
+        private readonly string collectionId;
+        private readonly DocumentClient cosmosDbClient;
+
+        public CosmosDBStore(
+            string dbEndpointUrl,
+            string dbAuthorizationKey,
+            string dbId,
+            string dbCollection,
+            string preferredLocation = null)
+        {
+            this.url = dbEndpointUrl;
+            this.primaryKey = dbAuthorizationKey;
+            this.databaseId = dbId;
+            this.collectionId = dbCollection;
+
+            ConnectionPolicy connectionPolicy = new ConnectionPolicy
+            {
+                ConnectionMode = ConnectionMode.Direct,
+                ConnectionProtocol = Protocol.Tcp,
+            };
+
+            if (!string.IsNullOrEmpty(preferredLocation))
+            {
+                connectionPolicy.PreferredLocations.Add(preferredLocation);
+            }
+
+            this.cosmosDbClient = new DocumentClient(new Uri(this.url), this.primaryKey, connectionPolicy);
+            this.cosmosDbClient.OpenAsync().Wait();
+        }
+    }
+}
---
+++ GraphView/Transaction/Transaction.cs
@@ -0,0 +1,118 @@
+﻿
+
+namespace GraphView.Transaction
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using GraphView.GraphViewDBPortal;
+    using Newtonsoft.Json.Linq;
+
+    internal enum OperationType
+    {
+        Insert,
+        Delete,
+        Update
+    }
+
+    internal class TransactionEntry : Tuple<object, OperationType, long>
+    {
+        internal TransactionEntry(object record, OperationType otype, long seqNum)
+            : base(record, otype, seqNum) { }
+
+        internal object Record
+        {
+            get
+            {
+                return this.Item1;
+            }
+        }
+
+        internal OperationType OperationType
+        {
+            get
+            {
+                return this.Item2;
+            }
+        }
+
+        internal long SequenceNumber
+        {
+            get
+            {
+                return this.Item3;
+            }
+        }
+    }
+
+    public class Transaction
+    {
+        /// <summary>
+        /// Data store for logging
+        /// </summary>
+        private readonly LogStore logStore;
+        /// <summary>
+        /// Transaction sequence number assigned to this transaction
+        /// </summary>
+        private readonly long sequenceNumber;
+
+        /// <summary>
+        /// A collection of records (indexed by their Ids) and the operations on them
+        /// </summary>
+        private Dictionary<string, List<TransactionEntry>> recordAccess;
+
+        public Transaction(LogStore logStore, long sequenceNumber)
+        {
+            this.logStore = logStore;
+            this.sequenceNumber = sequenceNumber;
+            this.recordAccess = new Dictionary<string, List<TransactionEntry>>();
+        }
+
+        public JObject ReadJson(string recordId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public JObject ReadJson(string recordId, JObject valueFromDataStore)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Tuple<string, JObject>> ReadJson(IEnumerable<string> ridList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Tuple<string, JObject>> ReadJson(IEnumerable<Tuple<string, JObject>> recordList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<JObject> ReadJson(RecordQuery recordQuery)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReadJsonString(string recordId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] ReadBytes(string recordId)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void WriteJson(JObject record)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteJson(IList<JObject> recordList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
