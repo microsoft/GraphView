8a15ed070c32665a088bacdc67e922c8ce7e810c
ShuochengWang <wangshch5@gmail.com>
2018-04-07 10:17:58 +0000

    [Update] Add simple benchmark.

---
+++ GraphViewBenchmark/Benchmark.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphView;
+using StartAzureBatch;
+
+namespace GraphViewBenchmark
+{
+    public class Benchmark
+    {
+        public static void Main(string[] args)
+        {
+            string batchAccountName = ConfigurationManager.AppSettings["BatchAccountName"];
+            string batchAccountKey = ConfigurationManager.AppSettings["BatchAccountKey"];
+            string batchAccountUrl = ConfigurationManager.AppSettings["BatchAccountUrl"];
+            string storageAccountName = ConfigurationManager.AppSettings["StorageAccountName"];
+            string storageAccountKey = ConfigurationManager.AppSettings["StorageAccountKey"];
+            string poolId = "BenchmarkPool";
+            int virtualMachineNumber = 8;
+            string virtualMachineSize = "Medium"; // virtual machine size information: https://docs.microsoft.com/en-us/azure/cloud-services/cloud-services-sizes-specs
+
+            AzureBatchJobManager jobManager = new AzureBatchJobManager(batchAccountName, batchAccountKey, batchAccountUrl,
+                storageAccountName, storageAccountKey, poolId, virtualMachineNumber, virtualMachineSize);
+
+            //jobManager.DeletePool(); return;
+
+            string docDBEndPoint = ConfigurationManager.AppSettings["DocDBEndPoint"];
+            string docDBKey = ConfigurationManager.AppSettings["DocDBKey"];
+            string docDBDatabaseId = ConfigurationManager.AppSettings["DocDBDatabaseId"];
+            string docDBCollectionId = ConfigurationManager.AppSettings["DocDBCollectionId-TwitterLists"];
+
+            List<int> parallelismList = new List<int>() { 1, 4, 8 };
+
+            foreach (int parallelism in parallelismList)
+            {
+                List<NodePlan> nodePlans = new List<NodePlan>();
+                int id = 1;
+                for (int i = 0; i < parallelism; i++)
+                {
+                    List<string> partitionValues = new List<string>();
+                    HashSet<int> inValues = new HashSet<int>();
+                    for (int j = 0; j < LoadData.PARTITION_NUM / parallelism; j++)
+                    {
+                        partitionValues.Add(id.ToString());
+                        inValues.Add(id);
+                        id++;
+                    }
+                    while (i == parallelism - 1 && id < LoadData.PARTITION_NUM)
+                    {
+                        partitionValues.Add(id.ToString());
+                        inValues.Add(id);
+                        id++;
+                    }
+
+                    NodePlan plan = new NodePlan(
+                        LoadData.PARTITION_BY_KEY,
+                        partitionValues,
+                        new PartitionPlan(inValues, null));
+                    nodePlans.Add(plan);
+                }
+                GraphViewAzureBatchJob job = new GraphViewAzureBatchJob(parallelism, nodePlans, 
+                    docDBEndPoint, docDBKey, docDBDatabaseId, docDBCollectionId,
+                    LoadData.USE_REVERSE_EDGE, LoadData.PARTITION_BY_KEY, LoadData.SPILLED_EDGE_THRESHOLD_VIAGRAPHAPI);
+
+                job.Traversal = job.Command.g().V().Count();
+                List<string> results = jobManager.TestQuery(job);
+                foreach (var res in results)
+                {
+                    Console.WriteLine(res);
+                }
+            }
+        }
+    }
+}
---
+++ GraphViewBenchmark/LoadData.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphView;
+
+namespace GraphViewBenchmark
+{
+    internal class LoadData
+    {
+        public static bool USE_REVERSE_EDGE = true;
+        public static string PARTITION_BY_KEY = "partition";
+        public static int SPILLED_EDGE_THRESHOLD_VIAGRAPHAPI = 1;
+
+        public static int PARTITION_NUM = 100;
+
+        public static string CalculatePartition(int key)
+        {
+            return (key % PARTITION_NUM).ToString();
+        }
+
+        /// <summary>
+        /// Load graph from file.
+        /// Graph data set format:
+        ///  First line: number of vertices
+        ///  Second line: number of edges
+        ///  Following lines: with one edge per line. The meaning of the columns are: 
+        ///                   First column: ID of from node
+        ///                   Second column: ID of to node
+        /// </summary>
+        public static void LoadTwitterListsGraph()
+        {
+            string[] lines = File.ReadAllLines(@"..\..\GraphDataSet\twitter-lists.txt");
+
+            Debug.Assert(lines.Length >= 2);
+            int numVertex = int.Parse(lines[0]);
+            int numEdge = int.Parse(lines[1]);
+
+            List<Tuple<int, int>> edges = new List<Tuple<int, int>>();
+            for (int i = 2; i < lines.Length; i++)
+            {
+                string[] ids = lines[i].Split(new[] { " ", "\t", "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+                Debug.Assert(ids.Length == 2);
+                edges.Add(new Tuple<int, int>(int.Parse(ids[0]), int.Parse(ids[1])));
+            }
+            Debug.Assert(numEdge == edges.Count);
+
+            string docDBEndPoint = ConfigurationManager.AppSettings["DocDBEndPoint"];
+            string docDBKey = ConfigurationManager.AppSettings["DocDBKey"];
+            string docDBDatabaseId = ConfigurationManager.AppSettings["DocDBDatabaseId"];
+            string docDBCollectionId = ConfigurationManager.AppSettings["DocDBCollectionId-TwitterLists"];
+
+            //GraphViewConnection graphConnection = GraphViewConnection.ResetGraphAPICollection(
+            //    docDBEndPoint, docDBKey, docDBDatabaseId, docDBCollectionId,
+            //    USE_REVERSE_EDGE, SPILLED_EDGE_THRESHOLD_VIAGRAPHAPI, PARTITION_BY_KEY);
+            GraphViewConnection graphConnection = new GraphViewConnection(
+                docDBEndPoint, docDBKey, docDBDatabaseId, docDBCollectionId, GraphType.GraphAPIOnly,
+                USE_REVERSE_EDGE, SPILLED_EDGE_THRESHOLD_VIAGRAPHAPI, PARTITION_BY_KEY);
+            GraphViewCommand graphCommand = new GraphViewCommand(graphConnection);
+
+            // Add vertex
+            //for (int id = 1; id <= numVertex; id++)
+            //{
+            //    graphCommand.g().AddV("User").Property("id", id.ToString()).Property(PARTITION_BY_KEY, CalculatePartition(id)).Next();
+            //    Console.WriteLine($"Add vertex {id} successfully");
+            //}
+
+            // Add edge
+            foreach (Tuple<int, int> tuple in edges)
+            {
+                string source = tuple.Item1.ToString();
+                string sink = tuple.Item2.ToString();
+                graphCommand.g().V().Has("id", source).AddE("Follow").To(graphCommand.g().V().Has("id", sink)).Next();
+                Console.WriteLine($"Add Edge {source} -> {sink} successfully");
+            }
+        }
+    }
+}
