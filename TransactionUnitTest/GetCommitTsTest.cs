305a8ee7df34f59047f6a8eb137b2ab27c9c2dd6
ZihanJiang <962019583@qq.com>
2018-04-09 21:54:22 +0800

    [Update] Add the test for GetCommitTs phase, Validation phase and PostProcessing phase. Update the comments in transaction's Validate()  method.

--- TransactionUnitTest/GetCommitTsTest.cs
+++ TransactionUnitTest/GetCommitTsTest.cs
@@ -3,75 +3,95 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TransactionUnitTest
 {
+    /// <summary>
+    /// Test the function of the 'Choosing commit timestamp phase'
+    /// </summary>
     [TestClass]
     public class GetCommitTsTest : AbstractTransactionTest
     {
-        /// <summary>
-        /// Test the function of the 'Choosing commit timestamp phase'
-        /// </summary>
         [TestMethod]
-        public void TestGetCommitTs()
+        public void TestGetCommitTs1()
         {
             //case1: read-only
             this.SetUp();
             Transaction t1 = new Transaction(null, this.versionDb);
-            t1.Read(GetCommitTsTest.TABLE_ID, "key");
+            t1.Read(TABLE_ID, DEFAULT_KEY);
             bool isSuccess = t1.GetCommitTimestamp();
             Assert.AreEqual(true, isSuccess);
-            Assert.AreEqual(0, t1.CommitTs);
+            Assert.AreEqual(0L, t1.CommitTs);
+        }
 
+        [TestMethod]
+        public void TestGetCommitTs2()
+        {
             //case2: update
             this.SetUp();
-            Transaction t2 = new Transaction(null, this.versionDb);
-            t2.Read(GetCommitTsTest.TABLE_ID, "key");
-            t2.Update(GetCommitTsTest.TABLE_ID, "key", "value1");
-            t2.UploadLocalWriteRecords();
-            isSuccess = t2.GetCommitTimestamp();
+            Transaction t1 = new Transaction(null, this.versionDb);
+            t1.Read(TABLE_ID, DEFAULT_KEY);
+            t1.Update(TABLE_ID, DEFAULT_KEY, "value1");
+            t1.UploadLocalWriteRecords();
+            bool isSuccess = t1.GetCommitTimestamp();
             Assert.AreEqual(true, isSuccess);
-            Assert.AreEqual(1, t2.CommitTs);
+            Assert.AreEqual(1L, t1.CommitTs);
+        }
 
+        [TestMethod]
+        public void TestGetCommitTs3()
+        {
             //case3: read after other tx update
             this.SetUp();
-            Transaction t3 = new Transaction(null, this.versionDb);
-            t3.Read(GetCommitTsTest.TABLE_ID, "key");
-            t3.Update(GetCommitTsTest.TABLE_ID, "key", "value1");
-            t3.Commit();
-            Transaction t4 = new Transaction(null, this.versionDb);
-            t4.Read(GetCommitTsTest.TABLE_ID, "key");
-            isSuccess = t4.GetCommitTimestamp();
+            Transaction t1 = new Transaction(null, this.versionDb);
+            t1.Read(TABLE_ID, DEFAULT_KEY);
+            t1.Update(TABLE_ID, DEFAULT_KEY, "value1");
+            t1.Commit();
+            Transaction t2 = new Transaction(null, this.versionDb);
+            t2.Read(TABLE_ID, DEFAULT_KEY);
+            bool isSuccess = t2.GetCommitTimestamp();
             Assert.AreEqual(true, isSuccess);
-            Assert.AreEqual(1, t4.CommitTs);
+            Assert.AreEqual(1L, t2.CommitTs);
+        }
 
+        [TestMethod]
+        public void TestGetCommitTs4()
+        {
             //case4: update after other tx update
             this.SetUp();
-            Transaction t5 = new Transaction(null, this.versionDb);
-            t5.Read(GetCommitTsTest.TABLE_ID, "key");
-            t5.Update(GetCommitTsTest.TABLE_ID, "key", "value1");
-            t5.Commit();
-            Transaction t6 = new Transaction(null, this.versionDb);
-            t6.Read(GetCommitTsTest.TABLE_ID, "key");
-            t6.Update(GetCommitTsTest.TABLE_ID, "key", "value2");
-            isSuccess = t6.GetCommitTimestamp();
+            Transaction t1 = new Transaction(null, this.versionDb);
+            t1.Read(TABLE_ID, DEFAULT_KEY);
+            t1.Update(TABLE_ID, DEFAULT_KEY, "value1");
+            t1.Commit();
+            Transaction t2 = new Transaction(null, this.versionDb);
+            t2.Read(TABLE_ID, DEFAULT_KEY);
+            t2.Update(TABLE_ID, DEFAULT_KEY, "value2");
+            bool isSuccess = t2.GetCommitTimestamp();
             Assert.AreEqual(true, isSuccess);
-            Assert.AreEqual(2, t6.CommitTs);
+            Assert.AreEqual(2L, t2.CommitTs);
+        }
 
+        [TestMethod]
+        public void TestGetCommitTs5()
+        {
             //case5: test the effect of tx.maxCommitTsOfWrites
             this.SetUp();
-            this.versionDb.UpdateVersionMaxCommitTs(GetCommitTsTest.TABLE_ID, "key", 1, 5);
-            Transaction t7 = new Transaction(null, this.versionDb);
-            t7.Read(GetCommitTsTest.TABLE_ID, "key");
-            t7.Update(GetCommitTsTest.TABLE_ID, "key", "value1");
-            t7.UploadLocalWriteRecords();
-            isSuccess = t7.GetCommitTimestamp();
+            this.versionDb.UpdateVersionMaxCommitTs(TABLE_ID, DEFAULT_KEY, 1L, 5L);
+            Transaction t1 = new Transaction(null, this.versionDb);
+            t1.Read(TABLE_ID, DEFAULT_KEY);
+            t1.Update(TABLE_ID, DEFAULT_KEY, "value1");
+            t1.UploadLocalWriteRecords();
+            bool isSuccess = t1.GetCommitTimestamp();
             Assert.AreEqual(true, isSuccess);
-            Assert.AreEqual(6, t7.CommitTs);
+            Assert.AreEqual(6L, t1.CommitTs);
+        }
 
+        [TestMethod]
+        public void TestGetCommitTs6()
+        {
             //case6: test the effect of tx.CommitLowerBound
             this.SetUp();
-            Transaction t8 = new Transaction(null, this.versionDb);
-            this.versionDb.UpdateCommitLowerBound(t8.TxId, 5);
-            long t8CommitTs = this.versionDb.SetAndGetCommitTime(t8.TxId, 4);
-            Assert.AreEqual(5, t8CommitTs);
+            Transaction t1 = new Transaction(null, this.versionDb);
+            this.versionDb.UpdateCommitLowerBound(t1.TxId, 5L);
+            long t1CommitTs = this.versionDb.SetAndGetCommitTime(t1.TxId, 4L);
+            Assert.AreEqual(5L, t1CommitTs);
         }
     }
 }
---
+++ TransactionUnitTest/ValidationTest.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GraphView.Transaction;
+
+namespace TransactionUnitTest
+{
+    [TestClass]
+    public class ValidationTest : AbstractTransactionTest
+    {
+        [TestMethod]
+        public void TestValidation1()
+        {
+            //Test the function of UpdateVersionMaxCommitTs()
+            this.SetUp();
+            VersionEntry versionEntry = this.versionDb.UpdateVersionMaxCommitTs(TABLE_ID, DEFAULT_KEY, 1L, 5L);
+            Assert.AreEqual(5, versionEntry.MaxCommitTs);
+
+            Transaction t1 = new Transaction(null, this.versionDb);
+            t1.Read(TABLE_ID, DEFAULT_KEY);
+            t1.Delete(TABLE_ID, DEFAULT_KEY);
+            t1.UploadLocalWriteRecords();
+
+            versionEntry = this.versionDb.UpdateVersionMaxCommitTs(TABLE_ID, DEFAULT_KEY, 1L, 10L);
+            Assert.AreEqual(10L, versionEntry.MaxCommitTs);
+        }
+
+        [TestMethod]
+        public void TestValidation2()
+        {
+            //Test the function of UpdateCommitLowerBound()
+            this.SetUp();
+            Transaction t1 = new Transaction(null, this.versionDb);
+            Assert.AreEqual(0L, this.versionDb.GetTxTableEntry(t1.TxId).CommitLowerBound);
+
+            long t1CommitTs = this.versionDb.UpdateCommitLowerBound(t1.TxId, 5L);
+            Assert.AreEqual(5L, this.versionDb.GetTxTableEntry(t1.TxId).CommitLowerBound);
+            Assert.AreEqual(-1L, t1CommitTs);
+
+            this.versionDb.SetAndGetCommitTime(t1.TxId, 0L);
+            t1CommitTs = this.versionDb.UpdateCommitLowerBound(t1.TxId, 7L);
+            Assert.AreEqual(5L, this.versionDb.GetTxTableEntry(t1.TxId).CommitLowerBound);
+            Assert.AreEqual(5L, t1CommitTs);
+        }
+
+        [TestMethod]
+        public void TestValidation3()
+        {
+            //the current version entry has not been held by any transaction
+            //just perform range check
+            this.SetUp();
+            Transaction t1 = new Transaction(null, this.versionDb);
+            t1.Read(TABLE_ID, DEFAULT_KEY);
+            t1.UploadLocalWriteRecords();
+            this.versionDb.SetAndGetCommitTime(t1.TxId, 5L);
+            t1.CommitTs = 5L;
+
+            bool isSuccess = t1.Validate();
+            Assert.AreEqual(true, isSuccess);
+
+            VersionEntry versionEntry =
+                this.versionDb.ReplaceVersionEntry(TABLE_ID, DEFAULT_KEY, 1L, 0L, 4L, -1, -1, long.MaxValue);
+            isSuccess = t1.Validate();
+            Assert.AreEqual(false, isSuccess);
+        }
+
+        [TestMethod]
+        public void TestValidation4()
+        {
+            // The current version entry has been held by another concurrent transaction
+            // And the concurrent transaction's status is Aborted
+            this.SetUp();
+
+            Transaction t2 = new Transaction(null, this.versionDb);
+            t2.Read(TABLE_ID, DEFAULT_KEY);
+            t2.UploadLocalWriteRecords();
+
+            Transaction t1 = new Transaction(null, this.versionDb);
+            t1.Read(TABLE_ID, DEFAULT_KEY);
+            t1.Delete(TABLE_ID, DEFAULT_KEY);
+            t1.UploadLocalWriteRecords();
+            this.versionDb.UpdateTxStatus(t1.TxId, TxStatus.Aborted);
+
+            bool isSuccess = t2.Validate();
+            Assert.AreEqual(true, isSuccess);
+        }
+
+        [TestMethod]
+        public void TestValidation5()
+        {
+            // The current version entry has been held by another concurrent transaction
+            // And the concurrent transaction's status is Committed
+            this.SetUp();
+
+            Transaction t2 = new Transaction(null, this.versionDb);
+            t2.Read(TABLE_ID, DEFAULT_KEY);
+            t2.UploadLocalWriteRecords();
+
+            Transaction t1 = new Transaction(null, this.versionDb);
+            t1.Read(TABLE_ID, DEFAULT_KEY);
+            t1.Delete(TABLE_ID, DEFAULT_KEY);
+            t1.UploadLocalWriteRecords();
+            this.versionDb.SetAndGetCommitTime(t1.TxId, 5L);
+            t1.CommitTs = 5L;
+            this.versionDb.UpdateTxStatus(t1.TxId, TxStatus.Committed);
+
+            this.versionDb.SetAndGetCommitTime(t2.TxId, 4L);
+            t2.CommitTs = 4L;
+            bool isSuccess = t2.Validate();
+            Assert.AreEqual(true, isSuccess);
+
+            this.versionDb.SetAndGetCommitTime(t2.TxId, 6L);
+            t2.CommitTs = 6L;
+            isSuccess = t2.Validate();
+            Assert.AreEqual(false, isSuccess);
+        }
+
+        [TestMethod]
+        public void TestValidation6()
+        {
+            // The current version entry has been held by another concurrent transaction
+            // if its status is Ongoing, try to push its commitLowerBound
+            // the tx who is locking the version has not gotten its commitTs and I push its commitLowerBound successfully.
+            this.SetUp();
+
+            Transaction t2 = new Transaction(null, this.versionDb);
+            t2.Read(TABLE_ID, DEFAULT_KEY);
+            t2.UploadLocalWriteRecords();
+
+            Transaction t1 = new Transaction(null, this.versionDb);
+            t1.Read(TABLE_ID, DEFAULT_KEY);
+            t1.Delete(TABLE_ID, DEFAULT_KEY);
+            t1.UploadLocalWriteRecords();
+
+            this.versionDb.SetAndGetCommitTime(t2.TxId, 4L);
+            t2.CommitTs = 4L;
+            bool isSuccess = t2.Validate();
+            Assert.AreEqual(true, isSuccess);
+            Assert.AreEqual(5L, this.versionDb.GetTxTableEntry(t1.TxId).CommitLowerBound);
+        }
+
+        [TestMethod]
+        public void TestValidation7()
+        {
+            // The current version entry has been held by another concurrent transaction
+            // if its status is Ongoing, try to push its commitLowerBound
+            // push failed, the transaction holding the version entry has gotten the commit time
+            this.SetUp();
+
+            Transaction t2 = new Transaction(null, this.versionDb);
+            t2.Read(TABLE_ID, DEFAULT_KEY);
+            t2.UploadLocalWriteRecords();
+
+            Transaction t1 = new Transaction(null, this.versionDb);
+            t1.Read(TABLE_ID, DEFAULT_KEY);
+            t1.Delete(TABLE_ID, DEFAULT_KEY);
+            t1.UploadLocalWriteRecords();
+
+            this.versionDb.SetAndGetCommitTime(t1.TxId, 5L);
+            t1.CommitTs = 5L;
+
+            this.versionDb.SetAndGetCommitTime(t2.TxId, 4L);
+            t2.CommitTs = 4L;
+            bool isSuccess = t2.Validate();
+            Assert.AreEqual(true, isSuccess);
+            Assert.AreEqual(0L, this.versionDb.GetTxTableEntry(t1.TxId).CommitLowerBound);
+
+            this.versionDb.SetAndGetCommitTime(t2.TxId, 6L);
+            t2.CommitTs = 6L;
+            isSuccess = t2.Validate();
+            Assert.AreEqual(false, isSuccess);
+            Assert.AreEqual(0L, this.versionDb.GetTxTableEntry(t1.TxId).CommitLowerBound);
+        }
+    }
+}
