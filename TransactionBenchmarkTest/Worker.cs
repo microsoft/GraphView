8ec097f70f6d197a72ce14b7c2abb60432371789
Xueping Weng <wxping715@gmail.com>
2018-04-13 15:58:22 +0800

    [Add] Add worker for benchmark test

---
+++ TransactionBenchmarkTest/Worker.cs
@@ -0,0 +1,87 @@
+﻿namespace TransactionBenchmarkTest
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal class Worker : IDisposable
+    {
+        public static readonly int DEFAULT_QUEUE_SIZE = 10000;
+
+        private Task<object>[] txTaskQueue;
+
+        private int currTxId;
+
+        /// <summary>
+        /// The spinlock for the sync of task Queue
+        /// </summary>
+        private SpinLock spinLock;
+        /// <summary>
+        /// The status of current Worker, close the 
+        /// </summary>
+        internal bool Active { get; set; }
+
+        internal int TaskQueueSize { get; set; }
+
+        /// <summary>
+        /// A flag to declare whether the worker has producer and consumer at the same time:
+        /// (1) True: all tasks have been enqueued in advance, 
+        ///           no concurrent threads add tasks when the daemon thread is working
+        /// (2) False: both producer and consumer are working at the same time
+        /// </summary>
+        internal bool OnlyConsumer { get; set; } = true;
+
+        public Worker(int queueSize = -1)
+        {
+            this.TaskQueueSize = queueSize == -1 ? Worker.DEFAULT_QUEUE_SIZE : queueSize;
+            this.txTaskQueue = new Task<object>[this.TaskQueueSize];
+            this.currTxId = -1;
+            this.spinLock = new SpinLock();
+        }
+
+        internal void EnqueueTxTask(Task<object> task)
+        {
+            // there is only a consumer working
+            if (this.OnlyConsumer)
+            {
+                int taskId = this.currTxId + 1;
+                if (taskId >= this.TaskQueueSize)
+                {
+                    throw new IndexOutOfRangeException("The task queue is full now");
+                }
+
+                this.txTaskQueue[taskId] = task;
+                this.currTxId++;
+            }
+            else
+            {
+                // TODO
+            }
+        }
+
+        internal void Monitor()
+        {
+            while (this.Active)
+            {
+                if (this.OnlyConsumer)
+                {
+                    if (this.currTxId >= 0)
+                    {
+                        Task task = this.txTaskQueue[this.currTxId--];
+                        task.Start();
+                        task.Wait();
+                    }
+                }
+                else
+                {
+                    // TODO
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Active = false;
+        }
+    }
+}
---
+++ TransactionBenchmarkTest/YCSB/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TransactionBenchmarkTest.YCSB
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // start a worker
+            Worker worker = new Worker();
+            worker.Active = true;
+            Thread thread = new Thread(new ThreadStart(worker.Monitor));
+            thread.Start();
+
+            Func<object, object> action = (object obj) =>
+            {
+                Console.WriteLine("Thread {0} is running", Thread.CurrentThread.ManagedThreadId);
+                Tuple<int, int> tuple = new Tuple<int, int>((int) obj, 123);
+                return (object)tuple;
+            };
+
+            List<Task<object>> taskList = new List<Task<object>>();
+            for (int i = 0; i < 5; i++)
+            {
+                Task<object> t = new Task<object>(action, i);
+                taskList.Add(t);
+                worker.EnqueueTxTask(t);
+            }
+
+            Thread.Sleep(1000);
+            for (int i = 0; i < 5; i++)
+            {
+                if (taskList[i].IsCompleted)
+                {
+                    Console.WriteLine(taskList[i].Result);
+                }
+            }
+            // close the worker
+            worker.Active = false;
+        }
+    }
+}
