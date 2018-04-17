1c17d60ffb6b890bd35cf4c295160c88b379a4e8
Haiquan Wang (MSR Student-FA Talent) <v-haiwan@microsoft.com>
2018-04-17 11:55:34 +0800

    add tpcc test

---
+++ TransactionBenchmarkTest/TPCC/Example.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphView.Transaction;
+using Newtonsoft.Json;
+using System.Collections;
+using ServiceStack.Redis;
+
+namespace TransactionBenchmarkTest.TPCC
+{
+    
+    class Example
+    {
+        // dataset populating
+        static void LoadTables()
+        {
+            RedisClient redisClient = new RedisClient(Constants.RedisHost, Constants.RedisPort);   // for payment to create `c_last` index
+            redisClient.ChangeDb(Constants.RedisIndexDbN);
+
+            VersionDb redisVersionDb = RedisVersionDb.Instance;
+            redisVersionDb.CreateVersionTable(Constants.DefaultTbl, Constants.RedisDbN);
+
+            string baseDir = Constants.BaseDirOfDatasets;
+            string[] tables = Constants.TableNames;
+            TableCode[] codes = Constants.TableCodes;
+
+            for (int i = 0; i < tables.Length; i++)
+            {
+                Console.WriteLine("Loading table " + tables[i]);
+                var tablePath = baseDir + tables[i];
+                var csvReader = new System.IO.StreamReader(tablePath);
+                var code = codes[i];
+                string line;
+                var cnt = 0;
+                while ((line = csvReader.ReadLine()) != null)
+                {
+                    string[] columns = line.Split(Constants.Delimiter);
+                    for (int j = 0; j < columns.Length; j++) { columns[j] = columns[j].Substring(1, columns[j].Length - 2); }   // remove head/tail `"` 
+
+                    Tuple<string, string> kv = RecordGenerator.BuildRedisKV(code, columns, redisClient);
+
+                    // tx
+                    Transaction tx = new Transaction(null, redisVersionDb);
+                    try
+                    {
+                        string tmpRecord = (string)tx.ReadAndInitialize(Constants.DefaultTbl, kv.Item1);
+                        if (tmpRecord == null) tx.Insert(Constants.DefaultTbl, kv.Item1, kv.Item2);
+                        tx.Commit();
+                    }
+                    catch (TransactionException e) { }
+
+                    cnt++;
+                    if (cnt % 10000 == 0) Console.WriteLine("\tcnt={0}", cnt);
+                }
+                Console.WriteLine("Load {0} records", cnt);
+            }
+
+        }
+
+
+        // Load New Order workload
+        static Queue<NewOrderInParameters> LoadNewOrderWorkload()
+        {
+            Queue<NewOrderInParameters> queue = new Queue<NewOrderInParameters>();
+
+            // load workload
+            string filepath = Constants.NewOrderWorkloadPath;
+            var csvReader = new System.IO.StreamReader(filepath);
+            string line = null;
+            line = csvReader.ReadLine();    // ignore the first line
+
+            while ((line = csvReader.ReadLine()) != null)
+            {
+                string[] columns = line.Split(new string[] { Constants.WorkloadDelimiter }, StringSplitOptions.None);
+                for (int j = 0; j < columns.Length; j++) { columns[j] = columns[j].Substring(1); }
+                columns[columns.Length - 1] = columns[columns.Length - 1].Substring(0, columns[columns.Length - 1].Length - 1); // remove `"`
+
+                var no = new NewOrderInParameters
+                {
+                    timestamp = columns[0],
+                    W_ID = Convert.ToUInt32(columns[5]),
+                    D_ID = Convert.ToUInt32(columns[3]),
+                    C_ID = Convert.ToUInt32(columns[1]),
+                    OL_I_IDs = JsonConvert.DeserializeObject<uint[]>(columns[6]),
+                    OL_SUPPLY_W_IDs = JsonConvert.DeserializeObject<uint[]>(columns[4]),
+                    OL_QUANTITYs = JsonConvert.DeserializeObject<uint[]>(columns[2]),
+                    O_ENTRY_D = columns[7]
+                };
+                queue.Enqueue(no);
+            }
+
+            return queue;
+        }
+
+        // load payment workload
+        static Queue<PaymentInParameters> LoadPaymentWorkload()
+        {
+            RedisClient redisClient = new RedisClient(Constants.RedisHost, Constants.RedisPort);   // for payment to access `c_last` index
+            redisClient.ChangeDb(Constants.RedisIndexDbN);
+
+            Queue<PaymentInParameters> queue = new Queue<PaymentInParameters>();
+
+            // load workload
+            string filepath = Constants.PaymentWorkloadPath;
+            var csvReader = new System.IO.StreamReader(filepath);
+            string line = null;
+            line = csvReader.ReadLine();    // ignore the first line: header
+            while ((line = csvReader.ReadLine()) != null)
+            {
+                string[] columns = line.Split(new string[] { Constants.WorkloadDelimiter }, StringSplitOptions.None);
+                for (int j = 0; j < columns.Length; j++) { columns[j] = columns[j].Substring(1); }
+                columns[columns.Length - 1] = columns[columns.Length - 1].Substring(0, columns[columns.Length - 1].Length - 1); // remove `"`
+
+                var pm = new PaymentInParameters
+                {
+                    timestamp = columns[0],
+                    C_ID = (columns[1] == "" ? 0 : Convert.ToUInt32(columns[1])),
+                    C_LAST = columns[2],    // may be ""
+                    H_DATE = columns[3],
+                    C_D_ID = Convert.ToUInt32(columns[4]),
+                    D_ID = Convert.ToUInt32(columns[5]),
+                    W_ID = Convert.ToUInt32(columns[6]),
+                    C_W_ID = Convert.ToUInt32(columns[7]),
+                    H_AMOUNT = Convert.ToDouble(columns[8])
+                };
+                queue.Enqueue(pm);
+            }
+
+            return queue;
+        }
+
+        static void Main(string[] args)
+        {
+            // populate dataset
+            Console.WriteLine("Loading TPCC tables...");
+            LoadTables();
+
+            // workload test
+            VersionDb vdb = RedisVersionDb.Instance;
+            vdb.CreateVersionTable(Constants.DefaultTbl, Constants.RedisDbN);
+            RedisClient redisClient = new RedisClient(Constants.RedisHost, Constants.RedisPort);   // for payment to access `c_last` index
+            redisClient.ChangeDb(Constants.RedisIndexDbN);
+
+            var cntCommitted = 0;
+            var cntAborted = 0;
+
+            // New-Order
+            Console.WriteLine("Loading New-Order workloads...");
+            var noQueue = LoadNewOrderWorkload();
+
+            Console.WriteLine("Doing New-Order");
+            DateTime startDT = System.DateTime.Now;
+            foreach (var e in noQueue)
+            {
+                var tpccwl = new TPCCNewOrderWorkload(e, vdb, null);
+                var ret = (NewOrderOutput)tpccwl.Run();     // return to output
+                if (ret.txFinalStatus == TxFinalStatus.COMMITTED)
+                    cntCommitted++;
+                else if (ret.txFinalStatus == TxFinalStatus.ABORTED)
+                    cntAborted++;
+            }
+            DateTime endDT = System.DateTime.Now;
+            TimeSpan ts = endDT.Subtract(startDT);
+
+            Console.WriteLine("\t total:{0}, Commit:{1}, Abort:{2}", noQueue.Count, cntCommitted, cntAborted);
+            Console.WriteLine("\t throughput:{0}, abort rate:{1}", (cntCommitted + cntAborted) / ts.TotalSeconds, cntAborted / (cntCommitted + cntAborted));
+            Console.WriteLine();
+
+            cntCommitted = 0;
+            cntAborted = 0;
+
+            // Payment
+            Console.WriteLine("Loading Payment workloads...");
+            var pmQueue = LoadPaymentWorkload();
+
+            Console.WriteLine("Doing Payment");
+            startDT = System.DateTime.Now;
+            foreach (var e in pmQueue)
+            {
+                var tpccwl = new TPCCPaymentWorkload(e, vdb, redisClient);
+                var ret = (PaymentOutput)tpccwl.Run();
+                if (ret.txFinalStatus == TxFinalStatus.COMMITTED)
+                    cntCommitted++;
+                else if (ret.txFinalStatus == TxFinalStatus.ABORTED)
+                    cntAborted++;
+            }
+            endDT = System.DateTime.Now;
+            ts = endDT.Subtract(startDT);
+
+            Console.WriteLine("\t total:{0}, Commit:{1}, Abort:{2}", pmQueue.Count, cntCommitted, cntAborted);
+            Console.WriteLine("\t throughput:{0}, abort rate:{1}", (cntCommitted + cntAborted) / ts.TotalSeconds, cntAborted / (cntCommitted + cntAborted));
+            Console.WriteLine();
+
+
+            Console.WriteLine("Done.");
+        }
+
+    }
+
+
+
+}
---
+++ TransactionBenchmarkTest/TPCC/TPCCWorkload.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ServiceStack.Redis;
+using GraphView.Transaction;
+
+namespace TransactionBenchmarkTest.TPCC
+{
+    class NewOrderInParameters
+    {
+        public string timestamp;
+        public uint W_ID;
+        public uint D_ID;
+        public uint C_ID;
+        public uint[] OL_I_IDs;
+        public uint[] OL_SUPPLY_W_IDs;
+        public uint[] OL_QUANTITYs;
+        public string O_ENTRY_D;
+    }
+    class NewOrderOutput
+    {
+        public TxFinalStatus txFinalStatus;
+        public Tuple<string, int, char, double, double>[] itemsData;
+        public Tuple<double, double, uint, double> other;
+        public CustomerPayload cpl;
+    }
+
+    class PaymentInParameters
+    {
+        public string timestamp;
+        public uint W_ID;
+        public uint D_ID;
+        public uint C_ID;
+        public string C_LAST;
+        public uint C_D_ID;
+        public uint C_W_ID;
+        public string H_DATE;
+        public double H_AMOUNT;
+    }
+    class PaymentOutput
+    {
+        public TxFinalStatus txFinalStatus;
+        public WarehousePayload wpl;
+        public DistrictPayload dpl;
+        public CustomerPayload cpl;
+    }
+
+
+    abstract class TPCCWorkload
+    {
+        protected object inParams;
+        protected RedisClient redisClient;
+        protected VersionDb vdb;
+        public TPCCWorkload(object inParams, VersionDb vdb, RedisClient redisClient)
+        {
+            this.inParams = inParams;
+            this.vdb = vdb;
+            this.redisClient = redisClient;
+        }
+
+        public abstract object Run();
+    }
+
+    class TPCCNewOrderWorkload : TPCCWorkload
+    {
+        public TPCCNewOrderWorkload(object inParams, VersionDb vdb, RedisClient redisClient) : base(inParams, vdb, redisClient)
+        {
+        }
+
+        public override object Run()
+        {
+            NewOrderInParameters input = (NewOrderInParameters)this.inParams;
+            NewOrderOutput ret = new NewOrderOutput();
+            ret.txFinalStatus = TxFinalStatus.COMMITTED;
+            Transaction tx = new Transaction(null, vdb);
+            try
+            {
+                // check input
+                if (input.OL_I_IDs.Length <= 0
+                    || input.OL_I_IDs.Length != input.OL_QUANTITYs.Length
+                    || input.OL_I_IDs.Length != input.OL_SUPPLY_W_IDs.Length)
+                {
+                    throw new Exception("invalid input");
+                }
+
+                // all local or not
+                bool allLocal = true;
+                for (int i = 0; i < input.OL_I_IDs.Length; i++)
+                {
+                    allLocal = allLocal & input.OL_I_IDs[i] == input.W_ID;
+                }
+
+                // whether all items exist without a wrong item number
+                ItemPayload[] items = new ItemPayload[input.OL_I_IDs.Length];
+                for (int i = 0; i < input.OL_I_IDs.Length; i++)
+                {
+                    ItemPkey ipk = new ItemPkey { I_ID = input.OL_I_IDs[i] };
+                    var str = (string)tx.Read(Constants.DefaultTbl, ipk.ToString());
+                    if (str == null)
+                        throw new Exception("invalid item id");
+                    items[i] = JsonConvert.DeserializeObject<ItemPayload>(str);
+                }
+
+                // read Warehouse,District, Customer
+                WarehousePkey wpk = new WarehousePkey { W_ID = input.W_ID };
+                WarehousePayload wpl = JsonConvert.DeserializeObject<WarehousePayload>((string)tx.Read(Constants.DefaultTbl, wpk.ToString()));
+                double W_TAX = wpl.W_TAX;
+
+                DistrictPkey dpk = new DistrictPkey { D_ID = input.D_ID, D_W_ID = input.W_ID };
+                DistrictPayload dpl = JsonConvert.DeserializeObject<DistrictPayload>((string)tx.Read(Constants.DefaultTbl, dpk.ToString()));
+                double D_TAX = dpl.D_TAX;
+                uint D_NEXT_O_ID = dpl.D_NEXT_O_ID;
+
+                CustomerPkey cpk = new CustomerPkey { C_ID = input.C_ID, C_D_ID = input.D_ID, C_W_ID = input.W_ID };
+                CustomerPayload cpl = JsonConvert.DeserializeObject<CustomerPayload>((string)tx.Read(Constants.DefaultTbl, cpk.ToString()));
+                double C_DISCOUNT = cpl.C_DISCOUNT;
+
+                // insert order/new-order, update next-order-id
+                OrderPkey opk = new OrderPkey
+                {
+                    O_ID = D_NEXT_O_ID,
+                    O_D_ID = input.D_ID,
+                    O_W_ID = input.W_ID
+                };
+                OrderPayload opl = new OrderPayload
+                {
+                    O_C_ID = input.C_ID,
+                    O_ENTRY_D = input.O_ENTRY_D,
+                    O_CARRIER_ID = Constants.NullCarrierID,
+                    O_OL_CNT = (uint)input.OL_I_IDs.Length,
+                    O_ALL_LOCAL = Convert.ToUInt32(allLocal)
+                };
+                tx.ReadAndInitialize(Constants.DefaultTbl, opk.ToString());
+                tx.Insert(Constants.DefaultTbl, opk.ToString(), JsonConvert.SerializeObject(opl));
+
+                dpl.D_NEXT_O_ID = D_NEXT_O_ID + 1;
+                tx.Update(Constants.DefaultTbl, dpk.ToString(), JsonConvert.SerializeObject(dpl));
+
+                NewOrderPkey nopk = new NewOrderPkey
+                {
+                    NO_O_ID = D_NEXT_O_ID,
+                    NO_D_ID = input.D_ID,
+                    NO_W_ID = input.W_ID
+                };
+                tx.ReadAndInitialize(Constants.DefaultTbl, nopk.ToString());
+                tx.Insert(Constants.DefaultTbl, nopk.ToString(), Constants.PlaceHolder);
+
+                // insert order lines
+                Tuple<string, int, char, double, double>[] itemsData = new Tuple<string, int, char, double, double>[input.OL_I_IDs.Length];
+                var total = 0.0;
+                for (int i = 0; i < input.OL_I_IDs.Length; i++)
+                {
+                    var OL_NUMBER = (uint)i + 1;
+                    var OL_SUPPLY_W_ID = input.OL_SUPPLY_W_IDs[i];
+                    var OL_I_ID = input.OL_I_IDs[i];
+                    var OL_QUANTITY = input.OL_QUANTITYs[i];
+
+                    var I_NAME = items[i].I_NAME;
+                    var I_DATA = items[i].I_DATA;
+                    var I_PRICE = items[i].I_PRICE;
+
+                    // read & update stock info
+                    var spk = new StockPkey { S_I_ID = OL_I_ID, S_W_ID = OL_SUPPLY_W_ID };
+                    StockPayload spl = JsonConvert.DeserializeObject<StockPayload>((string)tx.Read(Constants.DefaultTbl, spk.ToString()));
+                    spl.S_YTD += OL_QUANTITY;
+                    if (spl.S_QUANTITY >= OL_QUANTITY + 10)
+                        spl.S_QUANTITY -= (int)OL_QUANTITY;
+                    else
+                        spl.S_QUANTITY += 91 - (int)OL_QUANTITY;
+                    spl.S_ORDER_CNT += 1;
+                    if (input.OL_SUPPLY_W_IDs[i] != input.W_ID) spl.S_REMOTE_CNT += 1;
+                    tx.Update(Constants.DefaultTbl, spk.ToString(), JsonConvert.SerializeObject(spl));
+
+                    var OL_AMOUNT = OL_QUANTITY * I_PRICE;
+                    total += OL_AMOUNT;
+
+                    // insert order line
+                    OrderLinePkey olpk = new OrderLinePkey
+                    {
+                        OL_O_ID = D_NEXT_O_ID,
+                        OL_D_ID = input.D_ID,
+                        OL_W_ID = input.W_ID,
+                        OL_NUMBER = OL_NUMBER
+                    };
+                    tx.ReadAndInitialize(Constants.DefaultTbl, olpk.ToString());
+
+                    OrderLinePayload olpl = new OrderLinePayload
+                    {
+                        OL_I_ID = OL_I_ID,
+                        OL_SUPPLY_W_ID = OL_SUPPLY_W_ID,
+                        OL_DELIVERY_D = null,
+                        OL_QUANTITY = OL_QUANTITY,
+                        OL_AMOUNT = OL_AMOUNT,
+                        OL_DIST_INFO = spl.S_DIST_01        // TODO, assign to S_DIST_XX, where XX equals to D_ID
+                    };
+                    tx.Insert(Constants.DefaultTbl, olpk.ToString(), JsonConvert.SerializeObject(olpl));
+
+                    // add to return
+                    var brand = (I_DATA.Contains("ORIGINAL") && spl.S_DATA.Contains("ORIGINAL")) ? 'B' : 'G';
+                    itemsData[i] = new Tuple<string, int, char, double, double>(I_NAME, spl.S_QUANTITY, brand, I_PRICE, OL_AMOUNT);
+                }
+
+                // to return
+                total *= (1 - C_DISCOUNT) * (1 + W_TAX + D_TAX);
+                ret.other = new Tuple<double, double, uint, double>(W_TAX, D_TAX, D_NEXT_O_ID, total);
+                ret.itemsData = itemsData;
+                ret.cpl = cpl;
+
+                tx.Commit();
+            }
+            catch (Exception e)
+            {
+                tx.Abort();     // TODO is it right? if e is a TransactionException ?
+                ret.txFinalStatus = TxFinalStatus.COMMITTED;
+            }
+
+            return ret;
+        }
+    }
+
+
+    class TPCCPaymentWorkload : TPCCWorkload
+    {
+        public TPCCPaymentWorkload(object inParams, VersionDb vdb, RedisClient redisClient) : base(inParams, vdb, redisClient)
+        {
+        }
+
+        public override object Run()
+        {
+            PaymentInParameters input = (PaymentInParameters)this.inParams;
+            PaymentOutput ret = new PaymentOutput();
+            ret.txFinalStatus = TxFinalStatus.COMMITTED;
+            Transaction tx = new Transaction(null, vdb);
+            try
+            {
+                // determine c_id
+                var C_ID = input.C_ID;
+                if (C_ID == 0)  // by c_last
+                {
+                    var k = CustomerPayload.GetLastNameIndexKey(input.C_W_ID, input.C_D_ID, input.C_LAST);
+                    var ids = redisClient.GetAllItemsFromList(k);
+                    C_ID = Convert.ToUInt32(ids[ids.Count / 2]);    // TODO order by c_first?
+                }
+
+                var cpk = new CustomerPkey
+                {
+                    C_ID = C_ID,
+                    C_D_ID = input.C_D_ID,
+                    C_W_ID = input.C_W_ID
+                };
+                var cpl = JsonConvert.DeserializeObject<CustomerPayload>((string)tx.Read(Constants.DefaultTbl, cpk.ToString()));
+                cpl.C_BALANCE -= input.H_AMOUNT;
+                cpl.C_YTD_PAYMENT += input.H_AMOUNT;
+                cpl.C_PAYMENT_CNT += 1;
+                //var C_DATA = cpl.C_DATA;
+
+                // warehouse, district
+                var wpk = new WarehousePkey { W_ID = input.W_ID };
+                var wpl = JsonConvert.DeserializeObject<WarehousePayload>((string)tx.Read(Constants.DefaultTbl, wpk.ToString()));
+                wpl.W_YTD += input.H_AMOUNT;
+                tx.Update(Constants.DefaultTbl, wpk.ToString(), JsonConvert.SerializeObject(wpl));
+
+                var dpk = new DistrictPkey { D_ID = input.D_ID, D_W_ID = input.W_ID };
+                var dpl = JsonConvert.DeserializeObject<DistrictPayload>((string)tx.Read(Constants.DefaultTbl, dpk.ToString()));
+                dpl.D_YTD += input.H_AMOUNT;
+                tx.Update(Constants.DefaultTbl, dpk.ToString(), JsonConvert.SerializeObject(dpl));
+
+                // credit info
+                if (cpl.C_CREDIT == Constants.BadCredit)
+                {
+                    uint[] tmp = { C_ID, input.C_D_ID, input.C_W_ID, input.D_ID, input.W_ID };
+                    var newData = string.Join(" ", tmp) + " " + input.H_AMOUNT + "|" + cpl.C_DATA;
+                    if (newData.Length > Constants.Max_C_DATA)
+                    {
+                        newData = newData.Substring(0, Constants.Max_C_DATA);
+                    }
+                    cpl.C_DATA = newData;
+                }
+                tx.Update(Constants.DefaultTbl, cpk.ToString(), JsonConvert.SerializeObject(cpl));
+
+                // history
+                var hpl = new HistoryPayload
+                {
+                    H_C_ID = C_ID,
+                    H_C_D_ID = cpk.C_D_ID,
+                    H_C_W_ID = cpk.C_W_ID,
+                    H_D_ID = input.D_ID,
+                    H_W_ID = input.W_ID,
+                    H_DATA = wpl.W_NAME + "    " + dpl.D_NAME,
+                    H_AMOUNT = input.H_AMOUNT,
+                    H_DATE = input.timestamp
+                };
+                var hpk = HistoryPayload.GetHPkey();
+                tx.ReadAndInitialize(Constants.DefaultTbl, hpk);
+                tx.Insert(Constants.DefaultTbl, hpk, JsonConvert.SerializeObject(hpl));
+
+                // to return
+                ret.wpl = wpl;
+                ret.dpl = dpl;
+                ret.cpl = cpl;  // TODO C_ID may be null in input
+
+                tx.Commit();
+            }
+            catch (Exception e)
+            {
+                tx.Abort();
+                ret.txFinalStatus = TxFinalStatus.ABORTED;
+            }
+
+            return ret;
+        }
+    }
+
+
+}
