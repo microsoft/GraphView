ddb0040a88fd0b78702ac7d20eae1e4151745f08
Xueping Weng <wxping715@gmail.com>
2018-06-01 15:17:49 +0800

    [Optimize] optimize PartitionedVersionTableVisitor to reduce dict access times.

--- GraphView/Transaction/PartitionedVersionTableVisitor.cs
+++ GraphView/Transaction/PartitionedVersionTableVisitor.cs
@@ -16,13 +16,14 @@ namespace GraphView.Transaction
 
         internal override void Visit(DeleteVersionRequest req)
         {
-            if (dict.ContainsKey(req.RecordKey) && 
-                dict[req.RecordKey].ContainsKey(req.VersionKey))
+            Dictionary<long, VersionEntry> versionList = null;
+            if (dict.TryGetValue(req.RecordKey, out versionList) &&
+                versionList.ContainsKey(req.VersionKey))
             {
-                dict[req.RecordKey].Remove(req.VersionKey);
+                versionList.Remove(req.VersionKey);
 
                 // should reset the lastVersionKey, set the lastVersionKey as the current - 1
-                VersionEntry tailEntry = dict[req.RecordKey][VersionEntry.VERSION_KEY_STRAT_INDEX];
+                VersionEntry tailEntry = versionList[VersionEntry.VERSION_KEY_STRAT_INDEX];
                 tailEntry.BeginTimestamp -= 1;
             }
 
@@ -32,14 +33,15 @@ namespace GraphView.Transaction
 
         internal override void Visit(GetVersionListRequest req)
         {
-            if (!this.dict.ContainsKey(req.RecordKey))
+            Dictionary<long, VersionEntry> versionList = null;
+            if (!this.dict.TryGetValue(req.RecordKey, out versionList))
             {
                 req.Result = req.Container;
                 req.Finished = true;
                 return;
             }
 
-            VersionEntry tailPointer = this.dict[req.RecordKey][VersionEntry.VERSION_KEY_STRAT_INDEX];
+            VersionEntry tailPointer = versionList[VersionEntry.VERSION_KEY_STRAT_INDEX];
             long lastVersionKey = tailPointer.BeginTimestamp;
 
             TxList<VersionEntry> localList = req.Container;
@@ -51,7 +53,7 @@ namespace GraphView.Transaction
             {
                 // To make it run under .Net 4.5
                 VersionEntry verEntry = null;
-                this.dict[req.RecordKey].TryGetValue(lastVersionKey, out verEntry);
+                versionList.TryGetValue(lastVersionKey, out verEntry);
 
                 if (verEntry != null)
                 {
@@ -93,10 +95,12 @@ namespace GraphView.Transaction
 
         internal override void Visit(ReadVersionRequest req)
         {
-            if (this.dict.ContainsKey(req.RecordKey) && 
-                this.dict[req.RecordKey].ContainsKey(req.VersionKey))
+            Dictionary<long, VersionEntry> versionList = null;
+            VersionEntry verEntry = null;
+            if (this.dict.TryGetValue(req.RecordKey, out versionList) && 
+                versionList.TryGetValue(req.VersionKey, out verEntry))
             {
-                req.Result = this.dict[req.RecordKey][req.VersionKey];
+                req.Result = verEntry;
             }
             else
             {
@@ -107,13 +111,15 @@ namespace GraphView.Transaction
 
         internal override void Visit(ReplaceVersionRequest req)
         {
-            if (!this.dict.ContainsKey(req.RecordKey) || 
-                !this.dict[req.RecordKey].ContainsKey(req.VersionKey))
+            Dictionary<long, VersionEntry> versionList = null;
+            VersionEntry verEntry = null;
+
+            if (!this.dict.TryGetValue(req.RecordKey, out versionList) || 
+                !versionList.TryGetValue(req.VersionKey, out verEntry))
             {
                 throw new TransactionException("The specified version does not exist.");
             }
 
-            VersionEntry verEntry = this.dict[req.RecordKey][req.VersionKey];
             if (verEntry.TxId == req.ReadTxId && verEntry.EndTimestamp == req.ExpectedEndTs)
             {
                 verEntry.BeginTimestamp = req.BeginTs;
@@ -127,41 +133,45 @@ namespace GraphView.Transaction
 
         internal override void Visit(ReplaceWholeVersionRequest req)
         {
-            if (!this.dict.ContainsKey(req.RecordKey) ||
-                !this.dict[req.RecordKey].ContainsKey(req.VersionKey))
+            Dictionary<long, VersionEntry> versionList = null;
+
+            if (!this.dict.TryGetValue(req.RecordKey, out versionList) ||
+                !versionList.ContainsKey(req.VersionKey))
             {
                 throw new TransactionException("The specified version does not exist.");
             }
 
-            this.dict[req.RecordKey][req.VersionKey] = req.VersionEntry;
+            versionList[req.VersionKey] = req.VersionEntry;
             req.Result = true;
             req.Finished = true;
         }
 
         internal override void Visit(UpdateVersionMaxCommitTsRequest req)
         {
-            if (!this.dict.ContainsKey(req.RecordKey) ||
-                !this.dict[req.RecordKey].ContainsKey(req.VersionKey))
+            Dictionary<long, VersionEntry> versionList = null;
+            VersionEntry verEntry = null;
+
+            if (!this.dict.TryGetValue(req.RecordKey, out versionList) ||
+                !versionList.TryGetValue(req.VersionKey, out verEntry))
             {
                 throw new TransactionException("The specified version does not exist.");
             }
 
-            VersionEntry version = this.dict[req.RecordKey][req.VersionKey];
             // Only update the max commit time when uploaded commit time is larger than the version's
-            version.MaxCommitTs = Math.Max(version.MaxCommitTs, req.MaxCommitTs);
+            verEntry.MaxCommitTs = Math.Max(verEntry.MaxCommitTs, req.MaxCommitTs);
             
-            req.Result = version;
+            req.Result = verEntry;
             req.Finished = true;
         }
 
         internal override void Visit(UploadVersionRequest req)
         {
-            if (!this.dict.ContainsKey(req.RecordKey))
+            Dictionary<long, VersionEntry> versionList = null;
+            if (!this.dict.TryGetValue(req.RecordKey, out versionList))
             {
                 throw new TransactionException("The specified record does not exist.");
             }
 
-            Dictionary<long, VersionEntry> versionList = this.dict[req.RecordKey];
             if (versionList.ContainsKey(req.VersionKey))
             {
                 req.Result = false;  
@@ -170,7 +180,7 @@ namespace GraphView.Transaction
             {
                 versionList.Add(req.VersionKey, req.VersionEntry);
                 // Take the dirty version entry to store the current largest version key
-                VersionEntry tailEntry = this.dict[req.RecordKey][VersionEntry.VERSION_KEY_STRAT_INDEX];
+                VersionEntry tailEntry = versionList[VersionEntry.VERSION_KEY_STRAT_INDEX];
                 tailEntry.BeginTimestamp = req.VersionKey;
 
                 req.Result = true;
