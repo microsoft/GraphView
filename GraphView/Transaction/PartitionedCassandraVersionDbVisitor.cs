1ece2c77bcffd4edf0ef6f94fdbbd8c44d844e89
Liang Jeff Chen <jeche@microsoft.com>
2018-06-08 16:31:20 +0800

    Add code skeleton of partitioned Cassandra Db and Db visitor.

---
+++ GraphView/Transaction/PartitionedCassandraVersionDbVisitor.cs
@@ -0,0 +1,24 @@
+﻿
+namespace GraphView.Transaction
+{
+    class PartitionedCassandraVersionDbVisitor : VersionDbVisitor
+    {
+        private CassandraSessionManager SessionManager
+        {
+            get
+            {
+                return CassandraSessionManager.Instance;
+            }
+        }
+
+        internal override void Visit(NewTxIdRequest req)
+        {
+            base.Visit(req);
+        }
+
+        internal override void Visit(InsertTxIdRequest req)
+        {
+            base.Visit(req);
+        }
+    }
+}
