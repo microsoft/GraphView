2a1b09f234f30eb6cc68ee0bd53b89af7b63880e
VinaLx <vinaleux@gmail.com>
2018-09-03 14:53:43 +0800

    Add simple old version recycle logic

--- GraphView/Resources/RedisLua/UPLOAD_NEW_VERSION.lua
+++ GraphView/Resources/RedisLua/UPLOAD_NEW_VERSION.lua
@@ -8,14 +8,28 @@ if redis.call('HSETNX', hashKey, versionKey, payload) ~= 1 then
     return fail
 end
 
-local function CheckNewVersion(hashKey, newVersion)
-    local function BytesToInt(bytes)
-        local result = 0
-        for i = #bytes, 1, -1 do
-            result = result * 256 + string.byte(bytes, i)
-        end
-        return result
+local function BytesToInt(bytes)
+    local result = 0
+    for i = #bytes, 1, -1 do
+        result = result * 256 + string.byte(bytes, i)
     end
+    return result
+end
+
+local function IntToBytes(val)
+    local result = {}
+    for i = 1, 8 do
+        result[i] = val % 256
+        val = math.floor(val / 256)
+    end
+    return result
+end
+
+local function IntToByteString(val)
+    return string.char(unpack(IntToBytes(val)))
+end
+
+local function CheckNewVersion(hashKey, newVersion)
     local function Assert(b, message)
         if not b then
             error(message)
@@ -35,11 +49,23 @@ local function CheckNewVersion(hashKey, newVersion)
         ", hashKey " .. hashKey)
 end
 
--- CheckNewVersion(hashKey, versionKey)
+local function DeleteOneOldVersion(maxVersionListLength)
+    local oldVersion = BytesToInt(versionKey) - maxVersionListLength
+    if oldVersion < 0 then return end
+    redis.call('HDEL', hashKey, IntToByteString(oldVersion))
+end
+
+local function RecycleOldVersions()
+    local MAX_VERSION_LIST_LENGTH = 10
+    DeleteOneOldVersion(MAX_VERSION_LIST_LENGTH)
+end
+
+CheckNewVersion(hashKey, versionKey)
 
 -- field `LATEST_VERSION` store the lastest version number,
 -- usage see `GET_VERSION_LIST.lua`
 -- The correctness of this line relies on the lua-script caller
 redis.call('HSET', hashKey, 'LATEST_VERSION', versionKey)
+RecycleOldVersions()
 
 return success
