6fb7015c0c98f1019687d8e52cffa7ced3f76831
VinaLx <vinaleux@gmail.com>
2018-09-06 11:40:09 +0800

    Use HMGET to get two latest versions in GET_VERSION_LIST.lua.
    Now the script contains only 2 redis calls instead of 3.
    Thanks for the prompt by Xueping.

--- GraphView/Resources/RedisLua/GET_VERSION_LIST.lua
+++ GraphView/Resources/RedisLua/GET_VERSION_LIST.lua
@@ -24,32 +24,41 @@ local function IntToByteString(val)
     return string.char(unpack(IntToBytes(val)))
 end
 
+local function Assert(b, message)
+    if not b then error(message) end
+end
+
 -- entry doesn't exist
 local latestVersion = redis.call('HGET', hashKey, 'LATEST_VERSION')
-if latestVersion == false then
+
+Assert(
+    latestVersion ~= false,
+    "LATEST_VERSION field doesn't exist for hashKey " .. hashKey)
+
+-- LATEST_VERSION exists, but the version is not yet uploaded
+if #latestVersion ~= 8 then
     return {}
 end
 
-local latestPayload = redis.call('HGET', hashKey, latestVersion)
-local latestVersionNum = BytesToInt(latestVersion)
+local secondLatestVersion = IntToByteString(BytesToInt(latestVersion) - 1)
+local candidates = redis.call(
+    'HMGET', hashKey, latestVersion, secondLatestVersion)
 
 local result = nil
 
-local secondLatestVersion = IntToByteString(latestVersionNum - 1)
-local secondLatestPayload = redis.call('HGET', hashKey, secondLatestVersion)
-if secondLatestPayload == false then
-    result = {latestVersion, latestPayload}
+if candidates[1] == false then return {} end
+
+if candidates[2] == false then
+    result = {latestVersion, candidates[1]}
 else
     result = {
-        secondLatestVersion, secondLatestPayload, latestVersion, latestPayload}
+        secondLatestVersion, candidates[2], latestVersion, candidates[1]}
 end
 
 local function CheckVersionListResultIsValid(hashKey, result)
-    local function Assert(b, message)
-        if not b then error(message) end
-    end
     if #result == 2 then
-        return -- this case is passing for sure
+        Assert(BytesToInt(result[1]) == 0)
+        return
     end
     local ver2 = BytesToInt(result[1])
     local ver1 = BytesToInt(result[3])
