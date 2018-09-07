e75d32da39431b793ac8c3bc451eb5937b9f43d8
Xueping Weng <wxping715@gmail.com>
2018-09-07 18:10:09 +0800

    Fix the txRange issue, which causes imblananced partition for tx entry when the worker count is large.

--- GraphView/Transaction/TransactionRequest.cs
+++ GraphView/Transaction/TransactionRequest.cs
@@ -81,7 +81,7 @@
         Close,
     }
 
-       internal class TxRange
+    internal class TxRange
     {
         /// <summary>
         /// A tx executor, run by a thread, is assigned to a range (1,000,000) for tx Ids.
@@ -106,11 +106,27 @@
         internal static int GetRange(object txId)
         {
             int rangeWithOffset = (int)((long)txId / range);
-            return rangeWithOffset - (rangeWithOffset / RangeOffsetUnit) * RangeOffsetUnit;
+            // In multiple processes, here the computed range is a logical offset within all processes.
+            // Specific to a process, we should convert it to the local range to put the request into 
+            // right visitor.
+            // In the case, 6 workers runs in 2 processes(1-3 in process 1, 4-6 in process 2), RangeOffsetUnit=3. 
+            // The TxId offset in worker 5 will be 4, which is a logical range considering all workers. But each process has 
+            // only 3 workers, that is 3 visitors. Thus we convert it to the local offset as 4 - 3 = 1.
+            // If all workers run in the same process, RangeOffsetIndex will be 0.
+            return rangeWithOffset - RangeOffsetIndex * RangeOffsetUnit;
         }
 
         public TxRange(int start)
         {
+            // If workers runs in server processes, we should also control their txId intervals to avoid repeated txIds.
+            // For example, 6 workers runs in 2 processes(1-3 in process 1, 4-6 in process 2). Those workers belong to the 
+            // same process will have different txId intervals by the parameter RangeStart(always starts from 0), like 
+            // worker 1 starts from 0, worker 2 starts from 10000 etc. However, we should also control those workers' intervals
+            // in various processes by parameter RangeOffsetIndex, otherwise, worker 1 and worker 4 will have the same RangeStart
+            // in this case.Thus, here the RangeOffsetIndex of process 1 is 0, and it's 1 for process 2 to make intervals of workers
+            // 1-6 as 0, 10000, ..., 40000, 50000.
+            // If all workers are running in the same process, the RangeOffsetIndex will be the 
+            // default value 0.
             int startWithOffset = start + RangeOffsetUnit * RangeOffsetIndex;
             this.RangeStart = startWithOffset * TxRange.range;
             //this.RangeStart = start * TxRange.range;
